0eed239f021e8f6d31e36aa5e02d2264b7b39941
Ian Frosst <ianjfrosst@gmail.com>
2017-11-02 17:44:23 -0400

    Standard types

--- Autopilot/Src/main.c
+++ Autopilot/Src/main.c
@@ -130,10 +130,8 @@ int main(void)
   HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
 
-
   EEPROM_Init();
   
-
   /* USER CODE END 2 */
 
   /* Call init function for freertos objects (in freertos.c) */
