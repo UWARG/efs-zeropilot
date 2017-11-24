09545089eb4fa296f51efe5e09ff83837a348e16
Stephen John Cholvat <Stephen.cholvat@gmail.com>
2017-11-23 20:38:31 -0500

    tried DMA with main

--- Safety/Src/main.c
+++ Safety/Src/main.c
@@ -38,12 +38,12 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f0xx_hal.h"
+#include "dma.h"
 #include "i2c.h"
 #include "spi.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-#include "stm32f0xx_it.h"
 
 /* USER CODE BEGIN Includes */
 #include "debug.h"
@@ -96,6 +96,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_I2C1_Init();
   MX_SPI1_Init();
   MX_TIM1_Init();
@@ -119,7 +120,6 @@ int main(void)
   //HAL_GPIO_WritePin(BUZZER_GPIO_Port, BUZZER_Pin, GPIO_PIN_SET);
 
   interchipInit();
-  Spi1Flag = 1;
 
   /* USER CODE END 2 */
 
@@ -130,11 +130,11 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
-    if(Spi1Flag == 1){
+    /*if(Spi1Flag == 1){
       debug("Spi flag set");
       Spi1Flag = 0;
-    }
-    //pollInterchip();
+    }*/
+    pollInterchip();
 
   }
   /* USER CODE END 3 */
