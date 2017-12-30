4ea709d18c4cf33aad5972442a9c54d5246f8335
Ian Frosst <ianjfrosst@gmail.com>
2017-12-30 17:38:24 -0400

    Update file generation tool

--- Autopilot/Tools/file.py
+++ Autopilot/Tools/file.py
@@ -12,8 +12,8 @@ copy = '''\
  * @file {0}
  * @author {1}
  * @date {2}
- * @copyright Waterloo Aerial Robotics Group 2017 \\n
- *  https://raw.githubusercontent.com/UWARG/ZeroPilot-SW/master/LICENSE.md
+ * @copyright Waterloo Aerial Robotics Group {4} \\n
+ *  https://raw.githubusercontent.com/UWARG/ZeroPilot-SW/devel/LICENSE.md
  */\n
 '''
 
@@ -21,12 +21,12 @@ fnc = filename + '.c'
 fnh = filename + '.h'
 
 with open(dirn + 'Src/' + fnc, 'w') as f:
-    f.write(copy.format(fnc, author, datetime.date.today().isoformat()))
+    f.write(copy.format(fnc, author, datetime.date.today().isoformat(), datetime.date.today().year))
     f.write('#include "' + fnh + '"\n')
 
 
 with open(dirn + 'Inc/' + fnh, 'w') as f:
-    f.write(copy.format(fnh, author, datetime.date.today().isoformat()))
+    f.write(copy.format(fnh, author, datetime.date.today().isoformat(), datetime.date.today().year))
     defn = filename.upper() + '_H'
     f.write('#ifndef ' + defn + '\n')
     f.write('#define ' + defn + '\n' * 3)
