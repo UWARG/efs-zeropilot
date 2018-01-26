38393b2222c8b960748c90f7c8940ccb745407e6
Stephen Zhao <mail@zhaostephen.com>
2018-01-25 22:26:53 -0500

    Make build.sh better
    - migrated the $1 from the other build script, but as $@ so
      all extra args will be passed to cmake

--- Autopilot/Tools/build.sh
+++ Autopilot/Tools/build.sh
@@ -2,20 +2,27 @@
 
 # This script builds the ZeroPilot-SW Autopilot subproject
 
-echo "Building Autopilot..."
-
-mkdir -p build
-cd build
-cmake ../ -DCMAKE_TOOLCHAIN_FILE=STM32F765xG.cmake -G "Unix Makefiles"
-make
-
-if [[ $? != 0 ]]; then
+die() {
     echo ""
     echo "Autopilot build FAILED!"
-    echo "Error $? encountered by make"
-    exit $?
-fi
+    echo "Error $1 was encountered."
+    exit $1
+}
 
+# Prebuild info display
+echo "Building Autopilot..."
+if [[ $# > 0 ]]; then
+    echo "with cmake parameters: $@"
+fi
+echo ""
+
+# Build commands
+mkdir -p build || die $?
+cd build || die $?
+cmake ../ -DCMAKE_TOOLCHAIN_FILE=STM32F765xG.cmake -G "Unix Makefiles" $@ || die $?
+make || die $?
+
+# Final status display
 echo ""
 echo "Autopilot build SUCCESS!"
 exit 0
