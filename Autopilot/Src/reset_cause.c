c61a8de08f963242cdb2e8cb1f15523f2267da0a
James He <jamesatuw@gmail.com>
2018-03-13 18:01:21 -0400

    the working version of reset cause check

--- Autopilot/Src/reset_cause.c
+++ Autopilot/Src/reset_cause.c
@@ -4,9 +4,9 @@
 
 void reset_cause_check(){
     if(__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST)) debug("Pin reset");
-    else if(__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST)) debug("POR/PWD reset");
-    else if(__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST)) debug("Software reset");
-    else if(__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) debug("Independent watch dog rest");
-    else if(__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST)) debug("Window watch dog reset");
-    else if(__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST)) debug("Low power reset");
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST)) debug("POR/PWD reset");
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST)) debug("Software reset");
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) debug("Independent watch dog rest");
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST)) debug("Window watch dog reset");
+    if(__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST)) debug("Low power reset");
 }
