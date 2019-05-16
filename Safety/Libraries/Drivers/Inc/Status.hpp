a1258c77984dee3d8dcada515964ff55b6e35cdf
Serge Babayan <contact@sergebabayan.com>
2019-05-16 14:35:38 -0400

    Added ability to print out the reset cause of the mcu

--- Safety/Libraries/Drivers/Inc/Status.hpp
+++ Safety/Libraries/Drivers/Inc/Status.hpp
@@ -14,3 +14,25 @@ typedef enum StatusCode {
 	STATUS_CODE_INTERNAL_ERROR,
 	NUM_STATUS_CODES,
 } StatusCode;
+
+typedef enum ResetState {
+	RESET_STATE_WWDG = 0,
+	RESET_STATE_IWDG,
+	RESET_STATE_PWR,
+	RESET_STATE_SOFTWARE,
+	RESET_STATE_LOW_PWR,
+	RESET_STATE_PIN,
+	RESET_STATE_OPTION_BYTE_LOADER
+} ResetState;
+
+/**
+ * Gets the cause of the last reset from the MCU, and clears the CSR register
+ */
+ResetState get_reset_cause();
+
+/**
+ * Prints the reset state onto a buffer using sprintf
+ * @param buffer
+ * @param state
+ */
+void print_reset_state(char* buffer, ResetState state);
