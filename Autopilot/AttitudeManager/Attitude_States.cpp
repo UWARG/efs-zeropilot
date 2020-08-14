8ec6557ceb655b008a47ac019f24ea94377f2318
Sahil K <sahil.kale1@gmail.com>
2020-08-13 22:01:44 -0600

    Add in Lucy's attitudeStates

--- Autopilot/AttitudeManager/Attitude_States.cpp
+++ Autopilot/AttitudeManager/Attitude_States.cpp
@@ -2,11 +2,37 @@
 * Attitude State CPP file
 */
 #include "Attitude_States.hpp"
-
+#include "IMU.hpp"
+#include "airspeed.hpp"
+#include "PID.h"
+#include "OutputMixing.hpp"
+#include "SendInstructionsToSafety.hpp"
+#include "SensorFusion.hpp"
+/***********************************************************************************************************************
+ * Definitions
+ **********************************************************************************************************************/
 enum attitudeManagerState {
-    sensorInput,
-    pathManagerInput,
-    PIDloop,
+    pathManagerInputMode,
+    PIDloopMode,
     sendToSafetyMode,
-    failed
+    failedMode
 };
+attitudeManagerState currentState;
+/***********************************************************************************************************************
+ * Prototypes
+ **********************************************************************************************************************/
+static attitudeManagerState pathManagerInput(void);
+static attitudeManagerState PIDloop(void);
+static attitudeManagerState sendToSafety(void);
+static attitudeManagerState failed(void);
+/***********************************************************************************************************************
+ * Code
+ **********************************************************************************************************************/
+void initAttitudeState()
+{
+    //Initialize sensor classes via smart pointer 
+    //Initialize PID class
+    PIDController PID; 
+    //Initialize SendtoSafety Module
+    SendToSafety_Init();
+}
