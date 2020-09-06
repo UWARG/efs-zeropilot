cf604baf7034ecdba345f27e997d0250136ea841
Anthony Berbari <fberbari@edu.uwaterloo.ca>
2020-09-05 21:45:03 -0400

    Completed attitude managerdevelopement and unit testing

--- Autopilot/AttitudeManager/attitudeStateClasses.cpp
+++ Autopilot/AttitudeManager/attitudeStateClasses.cpp
@@ -1,8 +1,31 @@
 #include "attitudeStateClasses.hpp"
 
+/***********************************************************************************************************************
+ * Definitions
+ **********************************************************************************************************************/
+
+float OutputMixingMode::_channelOut[4];
+PMCommands fetchInstructionsMode::_PMInstructions;
+SFOutput_t sensorFusionMode::_SFOutput;
+PID_Output_t PIDloopMode::_PidOutput;
+
+/***********************************************************************************************************************
+ * Code
+ **********************************************************************************************************************/
+
 void fetchInstructionsMode::execute(attitudeManager* attitudeMgr)
 {
-    attitudeMgr->setState(sensorFusionMode::getInstance());
+
+    PMError_t ErrorStruct = PM_GetCommands(&_PMInstructions);
+
+    if (ErrorStruct.errorCode == 0)
+    {
+        attitudeMgr->setState(sensorFusionMode::getInstance());
+    }
+    else
+    {
+        attitudeMgr->setState(FatalFailureMode::getInstance());
+    }
 }
 
 attitudeState& fetchInstructionsMode::getInstance()
@@ -13,7 +36,16 @@ attitudeState& fetchInstructionsMode::getInstance()
 
 void sensorFusionMode::execute(attitudeManager* attitudeMgr)
 {
-    attitudeMgr->setState(PIDloopMode::getInstance());
+    SFError_t ErrorStruct = SF_GetResult(&_SFOutput, &ImuSens, &AirspeedSens);
+
+    if (ErrorStruct.errorCode == 0)
+    {
+        attitudeMgr->setState(PIDloopMode::getInstance());
+    }
+    else
+    {
+        attitudeMgr->setState(FatalFailureMode::getInstance());
+    }
 }
 
 attitudeState& sensorFusionMode::getInstance()
@@ -24,7 +56,22 @@ attitudeState& sensorFusionMode::getInstance()
 
 void PIDloopMode::execute(attitudeManager* attitudeMgr)
 {
-    attitudeMgr->setState(sendToSafetyMode::getInstance());
+
+    PMCommands *PMInstructions = fetchInstructionsMode::GetPMInstructions();
+    SFOutput_t *SFOutput = sensorFusionMode::GetSFOutput();
+
+    // TODO the IMU also gives us the measured derivative of roll, pitch, and yaw. That should be used in the PID algorithm
+    _PidOutput.rollPercent = _rollPid.PIDControl(PMInstructions->roll - SFOutput->IMUroll);
+    _PidOutput.pitchPercent = _pitchPid.PIDControl(PMInstructions->pitch - SFOutput->IMUpitch);
+    _PidOutput.yawPercent = _yawPid.PIDControl(PMInstructions->yaw - SFOutput->IMUyaw);
+    _PidOutput.throttlePercent = _airspeedPid.PIDControl(PMInstructions->airspeed - SFOutput->Airspeed);
+
+    _PidOutput.rollPercent = 0;  // TODO Pid needs fixing
+    _PidOutput.pitchPercent = 0;
+    _PidOutput.yawPercent = 0;
+    _PidOutput.throttlePercent = 50;
+
+    attitudeMgr->setState(OutputMixingMode::getInstance());
 }
 
 attitudeState& PIDloopMode::getInstance()
@@ -33,13 +80,59 @@ attitudeState& PIDloopMode::getInstance()
     return singleton;
 }
 
+void OutputMixingMode::execute(attitudeManager* attitudeMgr)
+{
+    PID_Output_t *PidOutput = PIDloopMode::GetPidOutput();
+
+    OutputMixing_error_t ErrorStruct = OutputMixing_Execute(PidOutput, _channelOut);
+
+    if (ErrorStruct.errorCode == 0)
+    {
+        attitudeMgr->setState(sendToSafetyMode::getInstance());
+    }
+    else
+    {
+        attitudeMgr->setState(FatalFailureMode::getInstance());
+    }
+
+}
+
+attitudeState& OutputMixingMode::getInstance()
+{
+    static OutputMixingMode singleton;
+    return singleton;
+}
+
 void sendToSafetyMode::execute(attitudeManager* attitudeMgr)
 {
-    attitudeMgr->setState(fetchInstructionsMode::getInstance());
+    float *channelOut = OutputMixingMode::GetChannelOut();
+
+    SendToSafety_error_t ErrorStruct = SendToSafety_Execute(channelOut);
+
+    if (ErrorStruct.errorCode == 0)
+    {
+        attitudeMgr->setState(fetchInstructionsMode::getInstance());
+    }
+    else
+    {
+        attitudeMgr->setState(FatalFailureMode::getInstance());
+    }
+
 }
 
 attitudeState& sendToSafetyMode::getInstance()
 {
     static sendToSafetyMode singleton;
+    return singleton;
+}
+
+void FatalFailureMode::execute(attitudeManager* attitudeMgr)
+{
+    attitudeMgr->setState(FatalFailureMode::getInstance());
+}
+
+attitudeState& FatalFailureMode::getInstance()
+{
+    static FatalFailureMode singleton;
     return singleton;
 }
--- Autopilot/AttitudeManager/attitudeStateClasses.hpp
+++ Autopilot/AttitudeManager/attitudeStateClasses.hpp
@@ -2,56 +2,126 @@
 #include "attitudeStateManager.hpp"
 #include "attitudeManager.hpp"
 
+#include "GetFromPathManager.hpp"
+#include "SensorFusion.hpp"
+#include "OutputMixing.hpp"
+#include "PID.h"
+#include "SendInstructionsToSafety.hpp"
+#include "IMU.hpp"
+#include "airspeed.hpp"
+
+/***********************************************************************************************************************
+ * Code
+ **********************************************************************************************************************/
+
+#ifdef SIMULATION
+
+#define IMU_CLASS SimulatedIMU
+#define AIRSPEED_CLASS SimulatedAirspeed
+
+#else
+
+#define IMU_CLASS ICM20602  // TODO to be replaced with the real classes once the sensor drivers are built
+#define AIRSPEED_CLASS dummyairspeed
+
+#endif
+/***********************************************************************************************************************
+ * Code
+ **********************************************************************************************************************/
+
+
 class fetchInstructionsMode : public attitudeState
 {
     public:
-        void enter(attitudeManager* attitudeMgr) {}
+        void enter(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         void execute(attitudeManager* attitudeMgr);
-        void exit(attitudeManager* attitudeMgr) {}
+        void exit(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         static attitudeState& getInstance();
+        static PMCommands *GetPMInstructions(void) {return &_PMInstructions;}
     private:
         fetchInstructionsMode() {}
         fetchInstructionsMode(const fetchInstructionsMode& other);
         fetchInstructionsMode& operator =(const fetchInstructionsMode& other);
+        static PMCommands _PMInstructions;
 };
 
 class sensorFusionMode : public attitudeState
 {
     public:
-        void enter(attitudeManager* attitudeMgr) {}
+        void enter(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         void execute(attitudeManager* attitudeMgr);
-        void exit(attitudeManager* attitudeMgr) {}
+        void exit(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         static attitudeState& getInstance();
+        static SFOutput_t *GetSFOutput(void) {return &_SFOutput;}
     private:
         sensorFusionMode() {}
         sensorFusionMode(const sensorFusionMode& other);
         sensorFusionMode& operator =(const sensorFusionMode& other);
+        IMU_CLASS ImuSens;
+        AIRSPEED_CLASS AirspeedSens;
+        static SFOutput_t _SFOutput;
+
 };
 
 class PIDloopMode : public attitudeState
 {
     public:
-        void enter(attitudeManager* attitudeMgr) {}
+        void enter(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         void execute(attitudeManager* attitudeMgr);
-        void exit(attitudeManager* attitudeMgr) {}
+        void exit(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         static attitudeState& getInstance();
+        static PID_Output_t *GetPidOutput(void) {return &_PidOutput;}
     private:
         PIDloopMode() {}
         PIDloopMode(const PIDloopMode& other);
         PIDloopMode& operator =(const PIDloopMode& other);
+        PIDController _rollPid{1, 0, 0, 100, 0};
+        PIDController _pitchPid{1, 0, 0, 100, 0};
+        PIDController _yawPid{1, 0, 0, 100, 0};
+        PIDController _airspeedPid{1, 0, 0, 100, 0};
+        static PID_Output_t _PidOutput;
+};
+
+class OutputMixingMode : public attitudeState
+{
+    public:
+        void enter(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
+        void execute(attitudeManager* attitudeMgr);
+        void exit(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
+        static attitudeState& getInstance();
+        static float *GetChannelOut(void) {return _channelOut;}
+    private:
+        OutputMixingMode() {}
+        OutputMixingMode(const OutputMixingMode& other);
+        OutputMixingMode& operator =(const OutputMixingMode& other);
+        static float _channelOut[4];
 };
 
 class sendToSafetyMode : public attitudeState
 {
     public:
-        void enter(attitudeManager* attitudeMgr) {}
+        void enter(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         void execute(attitudeManager* attitudeMgr);
-        void exit(attitudeManager* attitudeMgr) {}
+        void exit(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
         static attitudeState& getInstance();
     private:
         sendToSafetyMode() {}
         sendToSafetyMode(const sendToSafetyMode& other);
         sendToSafetyMode& operator =(const sendToSafetyMode& other);
 };
+
+class FatalFailureMode : public attitudeState
+{
+    public:
+        void enter(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
+        void execute(attitudeManager* attitudeMgr);
+        void exit(attitudeManager* attitudeMgr) {(void) attitudeMgr;}
+        static attitudeState& getInstance();
+    private:
+        FatalFailureMode() {}
+        FatalFailureMode(const FatalFailureMode& other);
+        FatalFailureMode& operator =(const FatalFailureMode& other);
+};
+
 
 //Reminder: add failed state mode?
