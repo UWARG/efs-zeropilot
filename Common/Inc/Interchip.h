48708c9aed7c02128e2756563a04b0e43897188d
dakaleyt <32375512+dakaleyt@users.noreply.github.com>
2020-11-19 19:17:33 -0500

    Fixing the Dumb Dumb

--- Common/Inc/Interchip.h
+++ Common/Inc/Interchip.h
@@ -3,7 +3,7 @@
 * Packets should be a multiple of 16 bits
 */
 
-/*
+
 typedef struct {
 	int16_t PWM[12];
 	uint16_t safety_level;
@@ -14,4 +14,3 @@ typedef struct {
 	uint16_t autonomous_level;
 } Interchip_AtoS_Packet;    //Autopilot to Safety packet
 
-*/
