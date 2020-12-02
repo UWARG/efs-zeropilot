db7a9a7b82ce352f92f24aabd66ee66e79452b00
Anthony Bertnyk <anthony.bertnyk@gmail.com>
2020-12-01 19:11:37 -0500

    Implement recieving of data from the attitude manager

--- Autopilot/PathManager/Src/CommWithAttitudeManager.cpp
+++ Autopilot/PathManager/Src/CommWithAttitudeManager.cpp
@@ -3,7 +3,17 @@
  * Author: Anthony Bertnyk
  */
 
-#include "../Inc/CommWithAttitudeManager.hpp"
+#include "CommWithAttitudeManager.hpp"
+
+extern "C"
+{
+#include "cmsis_os.h"
+}
+
+//Set up a mail queue for sending commands to the attitude manager
+const uint8_t COMMANDS_MAIL_Q_SIZE = 10;
+osMailQDef(commandsMailQ, COMMANDS_MAIL_Q_SIZE, CommandsForAM);
+osMailQId commandsMailQ;
 
 void CommWithAMInit()
 {
@@ -23,7 +33,29 @@ void SendCommandsForAM(CommandsForAM *Commands)
     osMailPut(commandsMailQ, commandsOut);
 }
 
+/*
+Set up a mail queue for recieving data from to the attitude manager.
+This may have to be moved to another file accessible by both the
+attitude manager and path manager interfaces.
+*/
+const uint8_t ATTITUDE_DATA_MAIL_Q_SIZE = 10;
+osMailQDef(attitudeDataMailQ, ATTITUDE_DATA_MAIL_Q_SIZE, AttitudeData);
+osMailQId attitudeDataMailQ;
+
 AttitudeData GetAttitudeData()
 {
-    
+    osEvent event;
+    AttitudeData * dataIn;
+    event = osMailGet(attitudeDataMailQ, osWaitForever);
+    if(event.status == osEventMail)
+    {
+        dataIn = static_cast<AttitudeData *>(event.value.p);
+    }
+
+    return *dataIn;
+}
+
+void FreeAttitudeData(AttitudeData *data)
+{
+    osMailFree(attitudeDataMailQ, data);
 }
