7bbb8cba84e894d56edb0198077a09bc06e35ddf
Aaditya <bluebird1339@gmail.com>
2020-12-22 20:01:19 -0500

    Added in Pulse Width and Interchip code
    - added pulse width to PPM.cpp
    - added in interchip code from safety-nuclear-changes

--- Safety/Src/PPM.cpp
+++ Safety/Src/PPM.cpp
@@ -7,7 +7,7 @@
 
 #define BASE_FREQUENCY 48000000
 #define SEC_TO_MICROSEC 1000000
-#define PULSE_WIDTH 0 //this should be in us
+#define PULSE_WIDTH 310 //this should be in us
 
 //number of milliseconds expected between subsequent PPM packets
 uint32_t PPM_PACKET_TIMEOUT = 3;
