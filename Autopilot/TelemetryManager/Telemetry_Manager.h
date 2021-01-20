8db384a35db1b96e35ef2600448829944a5ea2f4
JTing86 <jingting9786@gmail.com>
2021-01-19 20:31:16 -0500

    Telemetry (#76)
    Created Telemetry state machine structure, added Mavlink Lib2

---
+++ Autopilot/TelemetryManager/Telemetry_Manager.h
@@ -0,0 +1,23 @@
+/**
+ * @file Telemetry_Manager.h
+ * @author Jingting Liu
+ * @date January 14th, 2021
+ */
+
+
+#ifndef _TELEMETRY_STATE_MACHINE_H_
+#define _TELEMETRY_STATE_MACHINE_H_
+
+
+typedef enum TM_STATE_e {
+    INIT,
+    WAIT_FOR_GROUND_DATA,
+    CONNECT_WITH_PATH_MANAGER,
+    REGULAR_REPORT_READY,
+    ERR_REPORT_READY,
+    SEND_ERR_REPORT,
+    SEND_REGULAR_REPORT,
+} TM_STATE_e;
+
+void telemetry_manager(void);
+#endif
