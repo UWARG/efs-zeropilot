8c7138e6eea2e33ec66fe15218d531f2fa6c0402
Gfcello <gordonthecellist@gmail.com>
2021-02-12 22:11:03 -0500

    Initial Telemetry Framework
    Building files on Path manager and in Telemetry manager to allow communication between. Also made an interface in the Autopilot/Inc to hold the structs for the data being shared.

---
+++ Autopilot/TelemetryManager/CommWithPathManager.cpp
@@ -0,0 +1,58 @@
+/**
+ * Implements methods for communication with the path manager.
+ * Author: Anthony Bertnyk, messed with by Gordon Fountain to co-opt for Telemetry
+ */
+
+#include "CommWithPathManager.hpp"
+
+extern "C"
+{
+#include "cmsis_os.h"
+}
+
+void CommWithPMInit()
+{
+    PMcommandsMailQ = osMailCreate(osMailQ(commandsMailQ), NULL);
+}
+
+void SendCommandsForPM(CommandsForPM *commands)
+{
+    //Remove previous command from mail queue if it exists
+    osEvent event = osMailGet(PMcommandsMailQ, 0);
+    if(event.status == osEventMail)
+    {
+        osMailFree(PMcommandsMailQ, static_cast<CommandsForPM *>(event.value.p));
+    }
+
+    //Allocate mail slot
+    CommandsForPM *commandsOut;
+    commandsOut = static_cast<CommandsForPM *>(osMailAlloc(PMcommandsMailQ, osWaitForever));
+    
+    //Fill mail slot with data
+    *commandsOut = *commands;
+
+    //Post mail slot to mail queue
+    osMailPut(PMcommandsMailQ, commandsOut);
+}
+
+bool GetTelemData(TelemData *data)
+{
+    //Try to get data from mail queue
+    osEvent event;
+    TelemData * dataIn;
+    event = osMailGet(telemDataMailQ, 0);
+    if(event.status == osEventMail)
+    {
+        dataIn = static_cast<TelemData *>(event.value.p);
+        
+        //Keep the data and remove it from the queue
+        *data = *dataIn;
+        osMailFree(telemDataMailQ, dataIn);
+        return true;
+    }
+    else
+    {
+        //Indicate that no new data is available.
+        return false;
+    }
+}
