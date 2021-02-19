c41eea3737146d78766f7075d40a4496863932e1
Nixon Chan <nixonchan22@gmail.com>
2021-02-19 11:48:16 -0500

    creation of waypoints function

--- Autopilot/PathManager/Inc/landingManager.hpp
+++ Autopilot/PathManager/Inc/landingManager.hpp
@@ -1,24 +1,33 @@
 #include "waypointManager.hpp"
 
+struct _LandingPath
+{
+    _PathData stoppingPoint;
+    _PathData aimingPoint;
+    _PathData intersectionPoint;
+};
+
 class LandingManager : public WaypointManager{
 
     /*
         This function takes the horizontal position of the aircraft and determines the desired altitude
         Projects the position to the vertical plane relative to the aimingPoint, intersectionPoint, and stoppingPoint
-        Takes the horizontal coordinates, plug it into the line of the slope, get an altitude
+        Takes the one of the horizontal coordinates, plug it into the equation of the slope, get an altitude
     */
     double changingAltitude(_WaypointManager_Data_In * input, _PathData * aimingPoint, _PathData * intersectionPoint, _PathData * stoppingPoint);
 
     /*
         This function takes the current horizontal position of the plane and outputs a desired TRACK
         Looks at the plane in horizontal 2d, compares the position of the plane relative to the glide path
-    
+        ***
+        THIS FUNCTION MAY NOT BE NEEDED AS THE WAYPOINT MANAGER CAN PERFORM HORIZONTAL CALCULATIONS, WHILE THE ALTITUDE CAN BE MANAGED BY THE ABOVE FUNCTION
+        ***
     double horizontalAdjustment(_WaypointManager_Data_In input, _PathData * aimingPoint, _PathData * intersectionPoint);
     */
     /*
         This function sets the desired altitude so that the throttle turns off
     */
-    double throttleOff();
+    double throttleOff(void);
 
     /* 
         This function outputs the desired HEADING used in the decrab state
@@ -30,7 +39,7 @@ class LandingManager : public WaypointManager{
         This function takes the coordinates of the landing spots and direction of landing
         Creates stopping points, aiming point, and intersection point
     */
-    void createSlopeWaypoints(double stoppingLatitude, double stoppingLongitude, double stoppingAltitude, double directionLanding);
+    _LandingPath createSlopeWaypoints(_WaypointManager_Data_In * input, float stoppingLatitude, float stoppingLongitude, float stoppingAltitude, double directionLanding);
 
 };
 
--- Autopilot/PathManager/Src/landingManager.cpp
+++ Autopilot/PathManager/Src/landingManager.cpp
@@ -2,32 +2,35 @@
 #include "landingManager.hpp"
 #include <cmath>
 
-const double SET_THROTTLE_OFF = -1000;
+const int SET_THROTTLE_OFF = -1000;
+const double DISTANCE_OF_LANDING = 10; //in meters
+const int METERS_PER_DEG_LAT = 111320;
+const int ANGLE_OF_LANDING = 5; //in degrees
 
 double LandingManager::changingAltitude(_WaypointManager_Data_In * input, _PathData * aimingPoint, _PathData * intersectionPoint, _PathData * stoppingPoint)
 {
     //arrays to hold the important points
-    float aPoint[3]; //aming point
+    float aPoint[3]; //aiming point
     float iPoint[3]; //intersection point
     float sPoint[3]; //stopping point
     float cPoint[3]; //current point
     float projectedPoint[3];
 
     //loading important points
-    aPoint[0] = aimingPoint->latitude;
-    aPoint[1] = aimingPoint->longitude;
+    aPoint[0] = aimingPoint->longitude;
+    aPoint[1] = aimingPoint->latitude;
     aPoint[2] = aimingPoint->altitude; 
 
-    iPoint[0] = intersectionPoint->latitude;
-    iPoint[1] = intersectionPoint->longitude;
+    iPoint[0] = intersectionPoint->longitude;
+    iPoint[1] = intersectionPoint->latitude;
     iPoint[2] = intersectionPoint->altitude;
 
-    sPoint[0] = stoppingPoint->latitude;
-    sPoint[1] = stoppingPoint->longitude;
+    sPoint[0] = stoppingPoint->longitude;
+    sPoint[1] = stoppingPoint->latitude;
     sPoint[2] = stoppingPoint->altitude;
 
-    cPoint[0] = input->latitude;
-    cPoint[1] = input->longitude;
+    cPoint[0] = input->longitude;
+    cPoint[1] = input->latitude;
     cPoint[2] = input->altitude;
 
     //calculating the vectors
@@ -81,9 +84,11 @@ double LandingManager::horizontalAdjustment(_WaypointManager_Data_In input, _Pat
     //using follow waypoints algorithm, output the desired horizontal track
 }
 */
-double LandingManager::throttleOff()
+double LandingManager::throttleOff(void)
 {
     //return an altitude so that the throttle turns off
+    //I'm not sure if this is a good way to control throttle within the path manager
+    //Any suggestions?
     return SET_THROTTLE_OFF;
 }
 
@@ -94,13 +99,13 @@ double LandingManager::alignHeading(_PathData * aimingPoint, _PathData * interse
     float dVector[2];
     double heading;
 
-    aPoint[0] = aimingPoint->latitude;
-    aPoint[1] = aimingPoint->longitude;
+    aPoint[0] = aimingPoint->longitude;
+    aPoint[1] = aimingPoint->latitude;
 
-    iPoint[0] = intersectionPoint->latitude;
-    iPoint[1] = intersectionPoint->longitude;
+    iPoint[0] = intersectionPoint->longitude;
+    iPoint[1] = intersectionPoint->latitude;
 
-    //determine current heading and direction of landing
+    //determine heading and direction of landing from the aiming point and the landing point
     dVector[0] = aPoint[0] - iPoint[0];
     dVector[1] = aPoint[1] - iPoint[1];
 
@@ -140,8 +145,56 @@ double LandingManager::alignHeading(_PathData * aimingPoint, _PathData * interse
     return heading;
 }
 
-void LandingManager::createSlopeWaypoints(double stoppingLatitude, double stoppingLongitude, double stoppingAltitude, double directionLanding)
+_LandingPath LandingManager::createSlopeWaypoints(_WaypointManager_Data_In * input, float stoppingLatitude, float stoppingLongitude, float stoppingAltitude, double directionLanding)
 {
-    //load in the GPS locations of the important points
-    //setup waypoints
+    _LandingPath path;
+    path.stoppingPoint.latitude = stoppingLatitude;
+    path.stoppingPoint.longitude = stoppingLongitude;
+    path.stoppingPoint.altitude = stoppingAltitude;
+
+    //creating points from stopping point to aiming point 
+    
+    //setting Z of aiming point
+    path.aimingPoint.altitude = stoppingAltitude;
+    
+    //determining x and y of aiming point
+    double radianDirection = directionLanding * 180 / M_PI;
+
+    //finding the x and y components of the rolling distance vector
+    double stoppingDistX = sin(radianDirection) * DISTANCE_OF_LANDING;
+    double stoppingDistY = cos(radianDirection) * DISTANCE_OF_LANDING;
+
+    //converting into x and y components in lat and lon
+    float stoppingDistLat = stoppingDistY / METERS_PER_DEG_LAT;
+
+    float metersPerDegLon = 40075000 * cos(stoppingLatitude)/360;
+
+    float stoppingDistLon = stoppingDistX / metersPerDegLon;
+
+    //subtracting the distances from stopping point to get aiming point
+    path.aimingPoint.latitude = stoppingLatitude - stoppingDistLat;
+    path.aimingPoint.longitude = stoppingLongitude - stoppingDistLon;
+
+    //calculating the intersection point
+
+    //determining altitude of intersection
+    path.intersectionPoint.altitude = input->altitude;
+
+    //determining the horizontal distance of intersection
+    float horizDist = (input->altitude) / (ANGLE_OF_LANDING * M_PI / 180); //altitude in meters?
+
+    //finding the x and y components of the horizDist vector
+    double slopeDistX = sin(radianDirection) * horizDist;
+    double slopeDistY = cos(radianDirection) * horizDist;
+
+    //converting x and y components
+    float slopeDistLat = slopeDistY / METERS_PER_DEG_LAT;
+
+    float slopeDistLon = slopeDistX / metersPerDegLon; //same conversion factor used here because we are flying at a relatively small scale. Lat adjustments will affect it very minimally
+
+    //subtracting the distances from aiming point to get intersection point
+    path.intersectionPoint.latitude = path.aimingPoint.latitude - slopeDistLat;
+    path.intersectionPoint.longitude = path.aimingPoint.longitude - slopeDistLon;
+
+    return path;
 }
