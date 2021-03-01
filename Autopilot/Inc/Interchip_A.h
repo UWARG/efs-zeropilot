d9cc73efa97c56560e47bb0423d7a5911b48389a
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-02-28 19:28:02 -0500

    Add in guards

--- Autopilot/Inc/Interchip_A.h
+++ Autopilot/Inc/Interchip_A.h
@@ -1,11 +1,24 @@
+#ifndef INTERCHIP_A_H
+#define INTERCHIP_A_H
+
 #include "stm32f7xx_hal.h"
 #include "Interchip.h"
 #include <stdint.h>
 
 #define INTERCHIP_TRANSMIT_DELAY 5
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void Interchip_Run(void const *argument);
 int16_t *Interchip_GetPWM(void);
 void Interchip_SetPWM(int16_t *data);
 uint16_t Interchip_GetAutonomousLevel(void);
 void Interchip_SetAutonomousLevel(uint16_t data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
