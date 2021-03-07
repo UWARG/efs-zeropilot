233b4474d7afc9056cae3d242f2ff99f61edc338
Nixon Chan <nixonchan22@gmail.com>
2021-03-07 14:23:01 -0500

    comments and finetuning

--- Autopilot/PathManager/Src/landingSlopeStage.cpp
+++ Autopilot/PathManager/Src/landingSlopeStage.cpp
@@ -3,21 +3,24 @@
 
 void landingSlopeStage::execute(pathManager* pathMgr)
 {
-    if(sensorFusion::input.altitude<=(FLARE_ALTITUDE+getFromTelemetry::telemetryInput.stoppingAltitude))//altitude<=(FLARE_ALTITUDE+the ground altitude
+    if(sensorFusion::input.altitude <= (FLARE_ALTITUDE+getFromTelemetry::telemetryInput.stoppingAltitude)) //if less than flare altitude
     {
         pathMgr->stage = FLARE;
 
     }
     else
     {
+        //aligning horizontal position using waypointManager get_next_directions
         landingTransitionStage::waypointStatus = landingTransitionStage::landingPath.get_next_directions(getFromTelemetry::telemetryInput, &cruisingState::_outputdata);
+        //retrieving desired altitude for slope state and setting it 
         cruisingState::_outputdata.desiredAltitude = LandingManager::changingAltitude(getFromTelemetry::telemetryInput,landingTransitionStage::path.aimingPoint, landingTransitionStage::path.intersectionPoint, landingTransitionStage::path.stoppingPoint);
+        //retrieving desired speed for approach speed and setting it
         crusingState::_outputdata.desiredSpeed = LandingManager::approachSpeed(getFromTelemetry::telemetryInput.windSpeed, getFromTelemetry::telemetryInput.ifPackage);
     }
 
     if(landingTransitionStage::waypointStatus == INVALID_PARAMETERS)
     {
-        pathMgr->isError = true;
+        pathMgr -> isError = true;
     }
     
     if(pathMgr->isError)
