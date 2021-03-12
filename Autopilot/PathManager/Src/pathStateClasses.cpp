7b47c4ae478d0664a9be0623e99dd73f789f9afd
Nixon Chan <nixonchan22@gmail.com>
2021-03-12 13:55:20 -0500

    retrying build

--- Autopilot/PathManager/Src/pathStateClasses.cpp
+++ Autopilot/PathManager/Src/pathStateClasses.cpp
@@ -24,7 +24,7 @@ void getFromTelemetry::execute(pathManager* pathMgr)
     else
     {
         pathMgr -> setState(getSensorData::getInstance());
-    
+    }
 }
 
 pathManagerState& getFromTelemetry::getInstance()
