79fc36827a14ffcd8b7fe4c4a6a1f4be871db540
Nixon Chan <nixonchan22@gmail.com>
2021-03-25 13:51:19 -0400

    fixes and some comments

--- Autopilot/PathManager/Inc/landingTakeoffManager.hpp
+++ Autopilot/PathManager/Inc/landingTakeoffManager.hpp
@@ -3,8 +3,10 @@
 #include "pathDatatypes.hpp"
 #include "vectorClass.hpp"
 
+enum _FlightStage{ROLL=0, CLIMB, CRUISING, TRANSITION, SLOPE, FLARE, DECRAB, TOUCHDOWN};
+
 /************************
-    LANDING CONSTANTS
+    LANDING SECTION
 ************************/
 
 constexpr static double DISTANCE_OF_LANDING {10}; //in meters
@@ -16,17 +18,6 @@ constexpr static double PI {3.14159265358979323846};
 constexpr static int FLARE_ALTITUDE {2}; //in meters
 constexpr static double DECRAB_ALTITUDE {0.7}; //in meters
 constexpr static double TOUCHDOWN_ALTITUDE {0.05}; //in meters
-
-/************************
-    TAKEOFF CONSTANTS
-************************/
-
-constexpr static double EXIT_TAKEOFF_ALTITUDE {50}; //in meters, difference in altitude
-constexpr static double ROTATION_SPEED_NO_PACKAGE {4}; //in meters per second
-constexpr static double ROTATION_SPEED_WITH_PACKAGE {6}; //in meters per second
-constexpr static double CLIMB_SPEED_NO_PACKAGE {5}; // in meters per second
-constexpr static double CLIMB_SPEED_WITH_PACKAGE {7}; // in meters per second
-constexpr static double DISTANCE_OF_TAKEOFF {100}; // in meters, just used to approximate the horizontal location
 
 struct _LandingPath
 {
@@ -42,8 +33,6 @@ struct _LandingInitialData
     double stoppingAltitude;
     double landingDirection;
 };
-
-enum _FlightStage{ROLL=0, CLIMB, CRUISING, TRANSITION, SLOPE, FLARE, DECRAB, TOUCHDOWN};
 
 class LandingManager : public WaypointManager{
     
@@ -77,6 +66,17 @@ class LandingManager : public WaypointManager{
         static _LandingPath createSlopeWaypoints(Telemetry_PIGO_t input, double currentAltitude);
 
 };
+
+/************************
+    TAKEOFF SECTION
+************************/
+
+constexpr static double EXIT_TAKEOFF_ALTITUDE {50}; //in meters, difference in altitude
+constexpr static double ROTATION_SPEED_NO_PACKAGE {4}; //in meters per second
+constexpr static double ROTATION_SPEED_WITH_PACKAGE {6}; //in meters per second
+constexpr static double CLIMB_SPEED_NO_PACKAGE {5}; // in meters per second
+constexpr static double CLIMB_SPEED_WITH_PACKAGE {7}; // in meters per second
+constexpr static double DISTANCE_OF_TAKEOFF {100}; // in meters, just used to approximate the horizontal location
 
 class TakeoffManager : public WaypointManager{
 
