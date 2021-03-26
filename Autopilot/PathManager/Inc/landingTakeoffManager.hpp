805b896b0503556992dfdeaf788f7ab0c6089b3a
Nixon Chan <nixonchan22@gmail.com>
2021-03-25 20:09:36 -0400

    adding manual control struct and functionality

--- Autopilot/PathManager/Inc/landingTakeoffManager.hpp
+++ Autopilot/PathManager/Inc/landingTakeoffManager.hpp
@@ -25,6 +25,21 @@ struct _ManualControl
 
 enum _FlightStage{ROLL=0, CLIMB, CRUISING, TRANSITION, SLOPE, FLARE, DECRAB, TOUCHDOWN};
 
+struct _ManualControl
+{
+    double rollPercent;
+    bool rollManual;
+
+    double rudderPercent;
+    bool rudderManual;
+
+    double elevatorPercent;
+    bool elevatorManual;
+
+    double throttlePercent;
+    bool throttleManual;
+};
+
 /************************
     LANDING SECTION
 ************************/
