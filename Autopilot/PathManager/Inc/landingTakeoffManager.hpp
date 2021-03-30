d364f9e08017a7bf37911c9a81d6d44caaac06d8
Nixon Chan <nixonchan22@gmail.com>
2021-03-30 12:46:26 -0400

    manual control changes

--- Autopilot/PathManager/Inc/landingTakeoffManager.hpp
+++ Autopilot/PathManager/Inc/landingTakeoffManager.hpp
@@ -5,20 +5,6 @@
 
 enum _FlightStage{ROLL=0, CLIMB, CRUISING, TRANSITION, SLOPE, FLARE, DECRAB, TOUCHDOWN};
 
-struct _ManualControl
-{
-    double rollPercent;
-    bool rollManual;
-
-    double rudderPercent;
-    bool rudderManual;
-
-    double elevatorPercent;
-    bool elevatorManual;
-
-    double throttlePercent;
-    bool throttleManual;
-};
 
 /************************
     LANDING SECTION
