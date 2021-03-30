47015049990bbe6aa18a1f301c244cbd65c123f0
Nixon Chan <nixonchan22@gmail.com>
2021-03-30 11:57:47 -0400

    final fixes

--- Autopilot/PathManager/Inc/trackFinder.hpp
+++ Autopilot/PathManager/Inc/trackFinder.hpp
@@ -2,9 +2,11 @@
 
 
 /*
-    The goal of this function is to return the current track of the plane, given its velocities in the latitude and longitude direction.
-    Given the speeds of the aircraft in the latitude and longitude directions in m/s, this function will output the track in degrees.
-*/
+    The goal of this function is to return the current track of the plane in degrees, given its velocities in the latitude and longitude direction in m/s.
 
+    * @param[in] latitudeSpeed -> load in the speed of the plane in the N/S direction (change in latitude, m/s)
+    * @param[in] longitudeSpeed -> load in the speed of the plane in the E/W direction (change in longitude, m/s)
+    * @return double - > returns the direction of the resulting velocity/track of the plane
+*/
 double getTrack(double latitudeSpeed, double longitudeSpeed);
 
