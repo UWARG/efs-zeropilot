37ce52f29aaef4454b44ea0a08c1c5ee0947c28b
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-03-30 21:20:52 -0400

    Update altimeter.hpp

--- Autopilot/Inc/altimeter.hpp
+++ Autopilot/Inc/altimeter.hpp
@@ -6,22 +6,20 @@
 #ifndef ALTIMETER_HPP
 #define ALTIMETER_HPP
 
+#include <ctime>
+#include <cstdint>
+
 struct AltimeterData_t {
 
     float pressure, altitude, temp;
 
     bool isDataNew; 
     int status; //TBD but probably 0 = SUCCESS, -1 = FAIL, 1 = BUSY 
-    float utcTime; //Last time GetResult was called
+    uint32_t utcTime; //Last time GetResult was called
 };
 
 class Altimeter{
     public:
-        /**
-         * Initializes Altimeter
-         * */
-        virtual void Init() = 0; 
-
         /**
          * Triggers interrupt for new altimeter measurement - stores raw data in variables and returns right away
          * */
@@ -33,6 +31,25 @@ class Altimeter{
          * 3. Updates utcTime and status values in struct as well
          * */
         virtual void GetResult(AltimeterData_t *Data) = 0; //
+};
+
+class MS5637 : public Altimeter {
+    public:
+        MS5637(const MS5637*) = delete; //Apparently if you try to copy a singleton this will give you errors?
+        static MS5637* GetInstance();
+        void Begin_Measuring();
+        void GetResult(AltimeterData_t *Data);
+    private:
+        MS5637(); //Constructor can never be called muwhahaha
+        static MS5637* s_Instance;
+        uint32_t readFromMS5637(uint32_t commandToWrite);
+        void getRawPressureAndTemperature(float *displayPressure, float *displayTemperature, float *displayAltitude);
+        uint32_t getCurrentTime();
+        uint32_t timeOfResult;
+        bool dataIsNew = false;
+        float altitudeMeasured = 0, pressureMeasured = 0, temperatureMeasured = 0;
+
+
 };
 
 #endif
