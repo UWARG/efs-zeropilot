909ef5d2c980c466e0f34ac858363a2ac35d573f
Pop0097 <dhruvrawat09@gmail.com>
2021-04-03 11:37:54 -0400

    Added IMU header file

--- Autopilot/Inc/IMU.hpp
+++ Autopilot/Inc/IMU.hpp
@@ -1,10 +1,16 @@
 /**
  * IMU Sensor Functions and Part Number Selection
- * Author: Lucy Gong
+ * Author(s): Lucy Gong, Dhruv Rawat
  */
 
 #ifndef IMU_HPP
 #define IMU_HPP
+
+#include <stdlib.h>
+#include <ctime>
+#include <cstdint>
+#include <math.h>
+#include "main.h"
 
 #define ICM_20602 0
 #define MPU9255 1
@@ -13,16 +19,17 @@
 
 struct IMUData_t {
 
-    float magx, magy, magz;
+    float magx, magy, magz; //ICM20602 does not have a magnetometer. Thus, these readings will be left as NAN
     float accx, accy, accz;
     float gyrx, gyry, gyrz; 
+    float temp; 
 
-    bool isDataNew; 
+    bool isDataNew;
     int sensorStatus; //TBD but probably 0 = SUCCESS, -1 = FAIL, 1 = BUSY 
-    float utcTime; //Last time GetResult was called
+    uint32_t utcTime; //Last time GetResult was called
 };
 
-class IMU{
+class IMU {
     public:
         /**
          * Triggers interrupt for new IMU measurement - stores raw data in variables and returns right away
@@ -37,32 +44,44 @@ class IMU{
         virtual void GetResult(IMUData_t &Data) = 0; //
 };
 
-/***********************************************************************************************************************
- * Derived classes
- **********************************************************************************************************************/
-
 //To be replaced with implementation of actual sensor
 class ICM20602: public IMU{
     public:
-        /**
-         * Initializes IMU
-         * */
-        void Init(){}; 
+        ICM20602(const ICM20602*) = delete;
+        static ICM20602* GetInstance();
 
         /**
          * Triggers interrupt for new IMU measurement - stores raw data in variables and returns right away
          * */
-        void Begin_Measuring(){}; 
+        void Begin_Measuring(); 
 
         /**GetResult should:
          * 1. Reset dataIsNew flag
          * 2. Transfers raw data from variables to struct
          * 3. Updates utcTime and status values in struct as well
          * */
-        void GetResult(IMUData_t &Data){}; //
+        void GetResult(IMUData_t &Data); //
+             
+        
+    private:
+        //Constructor 
+        ICM20602();
 
+        //Object Instance
+        static ICM20602* imu_Instance;
+
+        //Variables
+        bool dataIsNew = false;
+        float accelConversionFactor, gyroConversionFactor, tempConversionFactor; //Conversion Factors
+        uint32_t imuSlaveIdentifier;
+        float measuredAccX = 0, measuredAccY = 0, measuredAccZ = 0; //Acceleration readings
+        float measuredGyroX = 0, measuredGyroY = 0, measuredGyroZ = 0; //Gyroscope readings 
+        float measuredTemp = 0; //Temperature reading
+
+        //Methods
+        void get_accel_temp_gyro_reading();
+        void write_data(uint8_t *);
 };
-
 
 #ifdef UNIT_TESTING
 #include "IMU_Mock.hpp"
@@ -70,15 +89,12 @@ class ICM20602: public IMU{
 
 #ifdef SIMULATION
 
-// This derived class hooks into the Simulink simulation rather than hardware
-class SimulatedIMU : public IMU
-{
-    public :
-        void Begin_Measuring(){};
+class SimulatedIMU : public IMU {
+    public:
+        void Begin_Measuring();
         void GetResult(IMUData_t &Data);
-    private:
-        float getNewestDataPoint(const char * fileName);
 };
+
 #endif
 
 #endif
