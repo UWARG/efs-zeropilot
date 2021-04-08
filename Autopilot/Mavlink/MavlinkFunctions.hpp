a70dcba2ffeb6ae67b4935420921d6791a2e9ed3
JTing86 <jingting9786@gmail.com>
2021-04-08 01:34:54 -0400

    added usage instructions

--- Autopilot/Mavlink/MavlinkFunctions.hpp
+++ Autopilot/Mavlink/MavlinkFunctions.hpp
@@ -35,7 +35,7 @@ typedef enum {
 
     while(byteavailable == true) // 50 is just a random number larger than message length (for GPS message length is 39)
     {
-        // get incoming byte here
+        // get incoming byte here, decoder can pick up the starting byte by itself
 
         if (decoderStatus != MAVLINK_DECODING_OKAY) //this make sure it stops after a whole message is received
         {
@@ -49,11 +49,21 @@ mavlink_decoding_status_t Mavlink_decoder(int channel, uint8_t incomingByte, uin
 /**
  * @brief Encode an selected struct
  *
- * @param type The type of telemetry data
- * @param msg The MAVLink message to compress the data into, this is guarenteed to be a full message starts from byte 0xfd
+ * @param type The type of telemetry data e.g. GPS or takeoff or landing...
+ * @param message The MAVLink message to compress the data into, this is guarenteed to be a full message starts from byte 0xfd
  * @param struct_ptr C-struct to read the message contents from
  * 
- * @return the length of the finalized message: msg->len + header_len + signature_len
+ * @return the status of encoding
+ * 
+ * Example usage, give the address of the struct you want to encode
+ *     mavlink_global_position_int_t global_position = 
+    {
+        ...
+    };
+
+    mavlink_message_t encoded_msg;
+
+    uint8_t encoderStatus = Mavlink_encoder(MESSAGE_ID_GPS, &encoded_msg, (const uint8_t*) &global_position);
  */
 mavlink_encoding_status_t Mavlink_encoder(Message_IDs_t type, mavlink_message_t *message, const uint8_t *struct_ptr);
 
