529d4278cdf70bef39256e39752aa1751fde09eb
Gfcello <78159714+Gfcello@users.noreply.github.com>
2021-05-03 19:25:04 -0400

    Telem and path manager comms (#116)
    * Adding comms files and interface header
    
    Note, interface header will be updated in the next commit
    
    * Adding the new data to PIGO and POGI
    
    * moving the cpp back to skeleton to make CI run
    
    * Update TelemPathInterface.hpp
    
    * Debugging to make Travis run
    
    * Update TelemPathInterface.hpp
    
    * Changing a function name attitude manager used

---
+++ Autopilot/Inc/TelemPathInterface.hpp
@@ -0,0 +1,60 @@
+/**
+ * Provides declarations common to the telemetry and path manager interfaces.
+ * Author: Anthony Bertnyk, Messed with by Gordon Fountain to turn from Attitude Manager to Telemetry
+ */
+ #include <cstdint>
+
+#ifndef TELEM_PATH_INTERFACE_HPP
+#define TELEM_PATH_INTERFACE_HPP
+
+//Data for path manager to send to telemetry.
+typedef struct POGI{
+	int errorCode; //Code for specified errors
+	double gpsLattitude,gpsLongitude,curAltitude; //Current gps position and altitude
+	float curAirspeed; //Airspeed data
+	float roll,pitch,yaw;	//Current orientation (radians)
+	float camRoll,camPitch,camYaw;	//Current camera orientation (radians)
+	bool isLanded; //Switch to check if landed (maybe limit switch?)
+	uint8_t editingFlightPathErrorCode,flightPathFollowingErrorCode; //Flight path error codes
+	uint8_t currentWaypointId, currentWaypointIndex; //current waypoint data
+	bool homeBaseInit; //is home base initialized
+} POGI;
+
+//Data for telemetry to send to path manager.
+typedef struct PIGO{
+	double gpsLattitude,gpsLongitude,altitude; //Commanded gps position and altitude
+	float landDirection,heading,distance; //Commanded heading/distance/landing direction
+	float pitch,yaw;	//Commanded orientation (radians)
+	bool beginLand; //Command to start landing process
+	bool beginTakeoff; //Starting command
+	uint8_t numWaypoints; //number of waypoints
+	uint8_t waypointModifyFlightPathCommand,waypointNextDirectionsCommand; //extra waypoint commands
+	uint8_t holdingAltitude, holdingTurnRadius,flightPathModifyNextId,flightPathModifyPrevId,flightPathModifyId; //flight path data
+	uint8_t holdingTurnDirection; //turn direction
+	//waypoint* waypoints; //list of waypoints (need to see if this is defined in waypoint code)
+	//homebase data (possibly constants?)
+	bool disconnectAutopilot; //Command to switch off autopilot
+} PIGO;
+
+//RTOS stuff that the cpp files need
+#ifdef TARGET_BUILD
+
+extern "C"
+{
+#include "cmsis_os.h"
+}
+
+const char MAIL_Q_SIZE = 1;
+
+//Set up a mail queue for sending commands to the path manager
+extern osMailQDef(PMcommandsMailQ, MAIL_Q_SIZE, PIGO);
+extern osMailQId PMcommandsMailQ;
+
+
+//Set up a mail queue for sending data to telemetry
+extern osMailQDef(telemDataMailQ, MAIL_Q_SIZE, POGI);
+extern osMailQId telemDataMailQ;
+
+#endif
+
+#endif
---
+++ Autopilot/PathManager/Src/CommsWithTelemetry.cpp
@@ -0,0 +1,61 @@
+/**
+ * Implements methods for sending path manager data to telemetry.
+ * Author: Anthony Bertnyk, Messed with for telemetry use by Gordon Fountain
+ */
+
+#include "CommsWithTelemetry.hpp"
+
+extern "C"
+{
+#include "cmsis_os.h"
+}
+
+void CommWithTelemInit()
+{
+    //telemDataMailQ = osMailCreate(osMailQ(telemDataMailQ), NULL);
+}
+
+void SendPathData(POGI *data)
+{
+    //Remove previous data from mail queue if it exists
+    /*osEvent event = osMailGet(telemDataMailQ, 0);
+    if(event.status == osEventMail)
+    {
+        osMailFree(telemDataMailQ, static_cast<POGI *>(event.value.p));
+    }
+
+    //Allocate mail slot
+    POGI *dataOut;
+    dataOut = static_cast<POGI *>(osMailAlloc(telemDataMailQ, osWaitForever));
+    
+    //Fill mail slot with data
+    *dataOut = *data;
+
+    //Post mail slot to mail queue
+    osMailPut(telemDataMailQ, dataOut);*/
+}
+
+bool GetCommands(POGI *commands)
+{
+    /*
+    //Try to get commands from mail queue 
+    osEvent event;
+    POGI * commandsIn;
+    event = osMailGet(commandsMailQ, 0);
+    if(event.status == osEventMail)
+    {
+        commandsIn = static_cast<POGI *>(event.value.p);
+        
+        //Keep the command and remove it from the queue
+        *commands = *commandsIn;
+        osMailFree(commandsMailQ, commandsIn);
+        return true;
+    }
+    else
+    {
+        //Indicate that no new commands are available.
+        return false;
+    }
+    */
+    return true;
+}
