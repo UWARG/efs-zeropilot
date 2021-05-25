4bd1fcc6fa33ba7735109558b15c52bee25196cc
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-05-24 21:58:35 -0400

    included vTaskDelayUntil into the code to allow for task timing with blinky

--- Autopilot/boardfiles/Src/freertos.c
+++ Autopilot/boardfiles/Src/freertos.c
@@ -79,9 +79,9 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
-static const int PERIOD_ATTITUDEMANAGER_MS = 20;
-static const int PERIOD_PATHMANAGER_MS = 10; //TO CONFIRM
-static const int PERIOD_TELEMETRY_MS = 10; //TO CONFIRM
+static const int PERIOD_ATTITUDEMANAGER_MS = 200;
+static const int PERIOD_PATHMANAGER_MS = 100; //TO CONFIRM
+static const int PERIOD_TELEMETRY_MS = 100; //TO CONFIRM
 
 /* USER CODE END Variables */
 osThreadId attitudeManagerHandle;
