ee98d77b65961bd50b1691c68a803bb9800fe0af
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-06-02 20:40:46 -0400

    added loop to allow for full execution of attitude manager state machine

--- Autopilot/AttitudeManager/Src/attitudeManagerInterface.cpp
+++ Autopilot/AttitudeManager/Src/attitudeManagerInterface.cpp
@@ -3,8 +3,21 @@
 #include "attitudeManager.hpp"
 
 static attitudeManager attMng;
+AttMan::_Attitude_Manager_Cycle_Status attManStatus;
+
 
 void AttitudeManagerInterfaceExecute(void)
 {
-    attMng.execute();
+
+    do
+        {
+            attMng.execute();
+            attManStatus = attMng.getStatus();
+
+            if (attManStatus == AttMan::FAILURE_MODE)
+            {
+                // Something is quite wrong, need to switch over to safety
+            }
+
+        } while(attManStatus != AttMan::COMPLETED_CYCLE);
 }
