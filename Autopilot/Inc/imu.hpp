a2eb94a11575f76092ba7f0e32c163c20c7e7db0
Anthony Berbari <fberbari@edu.uwaterloo.ca>
2021-06-05 17:27:10 -0400

    Completed the IMU driver

---
+++ Autopilot/Inc/imu.hpp
@@ -0,0 +1,94 @@
+/**
+ * IMU Sensor Functions and Part Number Selection.
+ * Available IMU driver(s): BMX160
+ * Author(s): Lucy Gong, Dhruv Rawat, Anthony Berbari
+ */
+
+#ifndef IMU_HPP
+#define IMU_HPP
+
+#include <cstdint>
+
+/***********************************************************************************************************************
+ * Definitions
+ **********************************************************************************************************************/
+
+struct IMUData_t {
+
+    float magx, magy, magz; // TODO although the BMX 160 has a magnetometer, it seems to produce bizarre results.More investigation needs to be done. Figuring out what the RHall register does is likely a part of that.
+    float accx, accy, accz;
+    float gyrx, gyry, gyrz; 
+    float temp; 
+
+    bool isDataNew;
+    int sensorStatus; // 0 = SUCCESS, -1 = FAIL, 1 = BUSY
+};
+
+/***********************************************************************************************************************
+ * Prototypes
+ *********************************************************************************************************************/
+
+class IMU {
+    public:
+        /**
+         * Begins a transaction with the IMU.
+         * This function is non blocking and returns right away, data will be stored inside the module as it arrives.
+         * To achieve synchronous data, this function must be called synchronously.
+         * */
+        virtual void Begin_Measuring() = 0; 
+
+        /**
+         * Retrieves any data already received by the imu.
+         * If no new data is available, the appropriate flag will be set in the return struct.
+         * All contents of the return struct, apart from the isDataNew flag, are undefined unless isDataNew is set to 1.
+         * This function is non blocking and returns right away.
+         * @param[in]       Data        reference to the results struct.
+         * */
+        virtual void GetResult(IMUData_t &Data) = 0;
+};
+
+class BMX160: public IMU{
+    public:
+
+        /**
+         * This module is built as a singleton. Thus to access a BMX160 object, this function must be called.
+         * Only a single BMX160 object will ever be created and will be shared by all callers of this function.
+         * @return      IMU        reference to the singleton object.
+         * */
+        static IMU& getInstance();
+
+        /**
+         * Deletes the constructor to disallow users to instantiate objects.
+         * */
+        BMX160(const BMX160*) = delete;
+
+        void Begin_Measuring();
+        void GetResult(IMUData_t &Data);
+
+        /**
+        * USERS SHOULD NEVER TOUCH THIS VARIABLE.
+        * It exists exclusively for use by an ISR internal to the module.
+        * Tampering with it from a higher level voids all guarantees made by this module.
+        * */
+        static bool dataIsNew;
+
+    private:
+
+        BMX160();
+
+        void SetAllPowerModesToNormal(void);
+        void ConfigAcc(void);
+        void ConfigGyro(void);
+        void ConfigMag(void);
+        void SetMagConfig(uint8_t regAddr, uint8_t data);
+        void PrepareMagForDataMode(void);
+
+        void Bmx160WriteReg(uint8_t reg, uint8_t val);
+        void Bmx160ReadReg(uint8_t const regAddr, uint8_t *pData, uint8_t len);
+
+        //Variables
+        uint8_t rawImuData[21];
+
+};
+
+#endif
--- Autopilot/boardfiles/Src/spi.c
+++ Autopilot/boardfiles/Src/spi.c
@@ -36,17 +36,17 @@ void MX_SPI1_Init(void)
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_MASTER;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_HARD_OUTPUT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
+  hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
   hspi1.Init.CRCPolynomial = 7;
   hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
   if (HAL_SPI_Init(&hspi1) != HAL_OK)
   {
     Error_Handler();
@@ -143,7 +143,6 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     /**SPI1 GPIO Configuration
     PD7     ------> SPI1_MOSI
     PG9     ------> SPI1_MISO
-    PG10     ------> SPI1_NSS
     PG11     ------> SPI1_SCK
     */
     GPIO_InitStruct.Pin = GPIO_PIN_7;
@@ -153,7 +152,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11;
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_11;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -269,12 +268,11 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* spiHandle)
     /**SPI1 GPIO Configuration
     PD7     ------> SPI1_MOSI
     PG9     ------> SPI1_MISO
-    PG10     ------> SPI1_NSS
     PG11     ------> SPI1_SCK
     */
     HAL_GPIO_DeInit(GPIOD, GPIO_PIN_7);
 
-    HAL_GPIO_DeInit(GPIOG, GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11);
+    HAL_GPIO_DeInit(GPIOG, GPIO_PIN_9|GPIO_PIN_11);
 
     /* SPI1 interrupt Deinit */
     HAL_NVIC_DisableIRQ(SPI1_IRQn);
