8007d0a915cff6ba228cb3f4d8da25eb3aade7f2
Aaditya Chaudhary <32117362+AadityaChaudhary@users.noreply.github.com>
2021-06-10 19:08:32 -0400

    Added Comments to function prototypes

--- Safety/Inc/interchip_S.hpp
+++ Safety/Inc/interchip_S.hpp
@@ -3,15 +3,24 @@
 #define INC_INTERCHIP_HPP_
 
 
+// Starts interchip interrupt. Call this once, not in a loop, each interchip callback
+// sets up another interrupt.
 void interchipInit();
 
+// Get the pwm signals that have been recieved. The length of the array returned will 
+// always be 12.
 volatile int16_t* getPWM();
 
+// Get the safety level that is currently being sent out.
 uint16_t getSafetyLevel();
+
+// Set the safety level that is currently being sent out.
 void setSafetyLevel(uint16_t level);
 
+// Return whether the data is new.
 bool isDataNew();
 
+// used to populate the interchip packet with fake data.
 void testSetup();
 
 #endif /* INC_INTERCHIP_HPP_ */
