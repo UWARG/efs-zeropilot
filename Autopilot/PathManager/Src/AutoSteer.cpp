e97a779616785ec32f510dac498d438abaf9ca35
Nixon Chan <68513632+NixonCTChan@users.noreply.github.com>
2021-06-12 23:37:25 -0400

    straight path follow works!

--- Autopilot/PathManager/Src/AutoSteer.cpp
+++ Autopilot/PathManager/Src/AutoSteer.cpp
@@ -12,13 +12,13 @@
  * Variables
  **********************************************************************************************************************/
 
-static PIDController bankPid{1, 3, 45, 5, -MAX_BANK_ANGLE, MAX_BANK_ANGLE}; // PID gains need to be tuned
+static PIDController bankPid{5,0,8,0, -MAX_BANK_ANGLE, MAX_BANK_ANGLE}; // 1, 3, 45, 5
 
-static PIDController rudderPid{1.5, 0, 20, 0, -100, 100}; // PID gains need to be tuned
+static PIDController rudderPid{0.2,0.2,7,1, -100, 100}; // 1.5, 0, 20, 0,
 
-static PIDController pitchPid{0.04, 0, 50, 0, -MAX_PITCH_ANGLE, MAX_PITCH_ANGLE}; // PID gains need to be tuned
+static PIDController pitchPid{1,0,0,0, -MAX_PITCH_ANGLE, MAX_PITCH_ANGLE}; // 0.04, 0, 50, 0,
 
-static PIDController altitudePid{25, 0, 200, 0, 0, 100}; //PID gains need to be tuned
+static PIDController altitudePid{110,0,0,0, 0, 100}; //25, 0, 200, 0,
 
 static const float RUDDER_SCALING_FACTOR = 0.5f; // should be experimentally determined
 
@@ -54,7 +54,7 @@ void AutoSteer_ComputeCoordinatedTurn(CoordinatedTurnInput_t *Input, Coordinated
     float bankAngle = bankPid.execute(0, directionError);
     float rudderSetPoint = GetRudderPercent(bankAngle);
 
-    float rudderCorrection = -1.0f * rudderPid.execute(0.0f, bankAngle);  // when accY is 0, the turn is coordinated. The multiplication by -1 comes from the way the axis is defined on the accelerometer.
+    float rudderCorrection = rudderPid.execute(0.0f, directionError);  // when accY is 0, the turn is coordinated. The multiplication by -1 comes from the way the axis is defined on the accelerometer.
 
     float rudderPercent = rudderCorrection;
 
@@ -65,10 +65,10 @@ void AutoSteer_ComputeCoordinatedTurn(CoordinatedTurnInput_t *Input, Coordinated
 
 void AutoSteer_ComputeAltitudeAndAirspeed(AltitudeAirspeedInput_t *Input, AltitudeAirspeedCommands_t *AttManCommands)
 {
-    float pitchAngle =-1*pitchPid.execute(Input->desiredAirspeed, Input->currentAirspeed);
+    float pitchAngle = pitchPid.execute(Input->desiredAltitude, Input->currentAltitude);
 
-    AttManCommands->requiredThrottlePercent = altitudePid.execute(Input->desiredAltitude, Input->currentAltitude);
-    AttManCommands->requiredPitch = DEG_TO_RAD(pitchAngle*AttManCommands->requiredThrottlePercent/100.0);
+    AttManCommands->requiredThrottlePercent = altitudePid.execute(Input->desiredAirspeed, Input->currentAirspeed);
+    AttManCommands->requiredPitch = DEG_TO_RAD(pitchAngle);
     
 
 }
