60997f1720e504e715d535023bcec71a580bb5d2
kgrewal26 <kaveetgrewal@gmail.com>
2021-06-17 21:30:55 -0400

    Fixed mainly formatting errors

--- Autopilot/Inc/timeStampingPOGI.hpp
+++ Autopilot/Inc/timeStampingPOGI.hpp
@@ -5,21 +5,51 @@
 #include <cstdlib>
 #include "gps.hpp"
 
-class timeStampingPOGI {
+class TimeStampingPOGI {
 
     public:
 
-        static timeStampingPOGI* GetInstance();
-        void setGPSTime(GpsData_t* gpsData); //need to change data type depending on what sensorFusion sends me
+        /*
+        *Called to either create or get the existing instance of the class
+        *Constructor is private so it only gets called through here (singleton)
+        */
+        static TimeStampingPOGI* GetInstance();
+
+        /*
+        * Called by sensor fusion to give the most recent gpsData struct
+        * This is used to update the recent UTC time in seconds
+        */
+        void setGPSTime(GpsData_t* gpsData); 
+
+        /*
+        * Call at any give time to receive the UTC time
+        * Given in milliseconds since midnight 
+        */
         uint32_t getUTCTimeMS();
 
     private:
 
-        timeStampingPOGI(); //constructor cant be called - singleton
-        static timeStampingPOGI* s_Instance;
+        /*
+        * Just the basic constructor - private so its a singleton
+        */
+        TimeStampingPOGI(); 
+
+        /*
+        * A pointer to the existing class instance - nullptr if no instance exists
+        */
+        static TimeStampingPOGI* s_Instance;
+
+        /*
+        * The most recent UTC time in seconds since midnight
+        */
         uint32_t recentUTCTime;
-        void initTimer();
+
 
 };
 
 #endif
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> Fixed mainly formatting errors
