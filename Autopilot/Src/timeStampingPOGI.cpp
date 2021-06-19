a1398377927c3cd483f307e4eed8a6ea28a0ed16
kgrewal26 <kaveetgrewal@gmail.com>
2021-06-19 01:21:35 -0400

    Switched to timer14 and improved some other code

--- Autopilot/Src/timeStampingPOGI.cpp
+++ Autopilot/Src/timeStampingPOGI.cpp
@@ -2,22 +2,19 @@
 #include "tim.h"
 #include "stm32f7xx_hal_tim.h"
 
+//Allocates and initializes the static data member pointer (not the object itself)
+TimeStampingPOGI* TimeStampingPOGI::s_Instance = nullptr;
 
 TimeStampingPOGI::TimeStampingPOGI(){
 
     //initializing and starting timer
-    MX_TIM12_Init();
-    HAL_TIM_Base_Init(&htim12);
-    HAL_TIM_Base_Start(&htim12);
+    HAL_TIM_Base_Init(&htim14);
+    HAL_TIM_Base_Start(&htim14);
 
 }
 
 
 //just typical singleton stuff
-
-//Allocates and initializes the static data member pointer (not the object itself)
-TimeStampingPOGI* TimeStampingPOGI::s_Instance = nullptr;
-
 //creates the instance if it doesnt exist & returns the pointer to the instance
 TimeStampingPOGI* TimeStampingPOGI::GetInstance(){
 
@@ -33,8 +30,16 @@ void TimeStampingPOGI::setGPSTime(GpsData_t* gpsData){
 
     //check if its new and set the recentUTCTime to it
     if (gpsData->timeIsNew){
+
         __HAL_TIM_SET_COUNTER(&htim12, 0);
-        recentUTCTime = gpsData->utcTime;
+
+        //gps data sends time in the form HHMMSS.00 - will use integer division to extract the hours, minutes and seconds indiviudually
+        int hours = gpsData->utcTime / 10000;
+        int mins = (gpsData->utcTime - (hours * 10000)) / 100;
+        int seconds = gpsData->utcTime - (hours * 10000) - (mins * 100);
+
+        //now get UTC time in seconds (3600 seconds in an hour & 60 in a minute)
+        recentUTCTime = (hours * 3600) + (mins * 60) + seconds;
     }
 
 }
@@ -43,7 +48,10 @@ void TimeStampingPOGI::setGPSTime(GpsData_t* gpsData){
 //call this to receive the milliseconds since midnight (in UTC)
 uint32_t TimeStampingPOGI::getUTCTimeMS(){
 
-    return (recentUTCTime * 1000) + __HAL_TIM_GET_COUNTER(&htim12);
+    //the timer is set to tick every 0.25 milliseconds
+    //thus, by dividing the number of ticks by 4, we get the milliseconds measured
+    //we only need millisecond precision so we dont really care about the truncation
+    return (recentUTCTime * 1000) + (__HAL_TIM_GET_COUNTER(&htim14) / 4);
 
 }
 
