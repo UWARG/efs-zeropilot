e09f224516f8ca5aceda4b94aac9814d30dbf094
Aaditya Chaudhary <32117362+AadityaChaudhary@users.noreply.github.com>
2021-06-20 20:25:32 -0400

    Implemented Airspeed
    airspeed driver uses ADC3.
    
    Polls the ADC10 times and averages the results.
    
    Accuracy of ~1 m/s based on my sketchy testing.

--- Autopilot/Inc/airspeed.hpp
+++ Autopilot/Inc/airspeed.hpp
@@ -13,8 +13,11 @@
 **/
 
 
+
 #ifndef AIRSPEED_HPP
 #define AIRSPEED_HPP
+
+#include <cstdint>
 
 /*
     Currently there is only one airspeed sensor used, if this is 
@@ -55,28 +58,57 @@ class airspeed {
  * Derived classes
  **********************************************************************************************************************/
 
-class dummyairspeed: public airspeed{
+class MPXV7002DP : public airspeed {
     public:
-        static dummyairspeed* GetInstance(); //Adding this early so the build works
+        MPXV7002DP(const MPXV7002DP*) = delete;
+        static MPXV7002DP* GetInstance();
+        /**GetResult should:
+         * Moves values from the variables to the airspeed data struct.
+         * Sets the dataNew variable to false after that.
+         */
+        void GetResult(airspeedData_t &Data);
 
         /**
-         *  Triggers interrupt for new airspeed measurement - stores 
-         *  raw data in variables and returns right away
-         * */
-        void Begin_Measuring() {}; 
+         *  Don't use this function, the GetResult function is non blocking.
+         */
+        void Begin_Measuring() {}
 
-        /**GetResult should:
-         *  1. Transfer raw data from variables to struct
-         *  2. Update utcTime and status values in struct as well
-         *  
-         *
-         *  Potentially:
-         *  ensure that data acquired makes sense, has been
-         *  gathered recently within reason (past 10s?)
-         * */
-        void GetResult(airspeedData_t &Data) {};
+        // Calculates the offset of the values coming from the sensor.
+        // It does this by taking 10 measurements, averaging them, and then subtracting
+        // the baseline value from them. The baseline voltage for no 
+        // differential pressure should be 2.5V.
+        void CalcOffset();
+        
+    private:
+        // Singleton style constructor.
+        MPXV7002DP();
+        // Single instance of the MPXV7002DP.
+        static MPXV7002DP* s_Instance; 
 
+        // Get the voltage from the diff pressure sensor, after applying the 
+        // offset. This value should be in the range of 0.0 to 5.0 (as that 
+        //is all the sensor can give).
+        float GetVoltage();
+        // Calculates the differential pressure from the voltage. This is done using the inverse of the 
+        // equation provided in the differential pressure sensor's datasheet. That equation is how the 
+        // sensor calculates the signal voltage from the pressure. So we use the inverse to calculate the
+        // pressure from the signal voltage.
+        float GetDiffPressure();
+        // Plugs the differential pressure into an equation to find airspeed. This is based on Bernoulli's equation.
+        float GetAirspeed();
+        // Gets the adc value 10 times, averages it, then returns that average.
+        uint16_t GetAverage();
+        // Returns the current time. This is used to get the time of a reading used in the airspeed data struct.
+       // uint32_t GetCurrentTime();
+
+        int offset = 0;
+        float airspeed = 0;
+        float utcTime = 0;
+        int sensorStatus = 0;
+        bool dataNew = false;
+        
 };
+
 
 #ifdef UNIT_TESTING
 #include "airspeed_Mock.hpp"
