a4a21d657a5eecfa673d164eaf6c78fb46757db1
Anthony Luo <anthonyluo100@Gmail.com>
2021-10-03 14:33:17 -0400

    add test

---
+++ Autopilot/Test/Inc/crc_Mock.hpp
@@ -0,0 +1,8 @@
+//
+// Created by antho on 10/3/2021.
+//
+
+#ifndef ZEROPILOT_SW_CRC_MOCK_H
+#define ZEROPILOT_SW_CRC_MOCK_H
+
+#endif //ZEROPILOT_SW_CRC_MOCK_H
---
+++ Autopilot/Test/Src/CRC/Test_CRC.cpp
@@ -0,0 +1,22 @@
+//
+// Created by antho on 10/3/2021.
+//
+
+#include <gtest/gtest.h>
+
+#include "crc_Mock.hpp"
+
+using namespace std;
+using ::testing::Test;
+
+// test correct CRC calculation for different arrays.
+
+// check that CRC is being sent properly
+
+
+
+// TODO: tests for safety
+// - Make sure CRC is being received?
+// - Verify that CRC is being calculated
+// - Verify that CRC is being cross-checked
+// - Verify that we will get kicked out if CRC is incorrect X times in a row
