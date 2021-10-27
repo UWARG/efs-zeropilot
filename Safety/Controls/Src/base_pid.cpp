202a26fb177fb00b48def982bdfc5b8e8b43731c
Anthony Luo <anthonyluo100@Gmail.com>
2021-10-27 18:22:33 -0400

    added controls.cpp

--- Safety/Controls/Src/base_pid.cpp
+++ Safety/Controls/Src/base_pid.cpp
@@ -2,3 +2,16 @@
 // Created by antho on 10/27/2021.
 //
 
+// Inputs will be setvalues for:
+//      - Roll  (Degree)
+//      - Pitch (Degree)
+//      - Yaw   (Degree)
+//      - Elevation (Change from prev, unsure?)
+
+// should output 4 PWM values for:
+//      - Left Front    (LF)
+//      - Right Front   (RF)
+//      - Left Rear     (LR)
+//      - Right Rear    (RR)
+
+// pika
