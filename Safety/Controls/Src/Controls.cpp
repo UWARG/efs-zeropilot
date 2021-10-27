db370da251786273c4f3a1c75be724c13c62e787
Anthony Luo <anthonyluo100@Gmail.com>
2021-10-27 18:28:40 -0400

    headers

--- Safety/Controls/Src/Controls.cpp
+++ Safety/Controls/Src/Controls.cpp
@@ -1,4 +1,16 @@
 //
+// Controls.cpp
 // Created by antho on 10/27/2021.
 //
+
+/* Inputs:
+ * - Safety IMU data (fast updates)
+ * - PM Localization data (to be confirmed)
+ * - Target values (currently just angles/vector -> Perhaps to be changed later)
+ * - Flight Status
+*/
+
+/* Outputs:
+ * Literally 4 pwm values :D
+*/
 
