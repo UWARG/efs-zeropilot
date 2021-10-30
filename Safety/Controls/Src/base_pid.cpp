775a9e34db0050547c5c9877fafbfd7da405f2c8
Anthony Luo <anthonyluo100@Gmail.com>
2021-10-30 17:55:59 -0400

    implement basic PID (ty ab)

--- Safety/Controls/Src/base_pid.cpp
+++ Safety/Controls/Src/base_pid.cpp
@@ -1,15 +1,73 @@
-//
-// Created by antho on 10/27/2021.
-//
-
-/*
- * Inputs:
- *      - Setval (target distance forward?)
- *      - IMU data (how much we have moved ? -> I don't think this is necessary but I'll double check).
- *
- * Outputs:
- *      - PWM Value (% of max)
+/**
+ * @file base_pid.cpp
+ * @authors Anthony Luo & (Ian Frosst, Anthony Berbari)
+ * @date October 30, 2021
+ * @copyright Waterloo Aerial Robotics Group 2021
  */
 
-// pika
+#include "base_pid.hpp"
 
+/***********************************************************************************************************************
+ * Code
+ **********************************************************************************************************************/
+
+PIDController::PIDController(float _kp, float _ki, float _kd, float _i_max, float _min_output, float _max_output){
+	kp = _kp;
+	kd = _kd;
+	ki = _ki;
+
+	i_max = _i_max;
+	min_output = _min_output;
+	max_output = _max_output;
+
+	integral = 0.0f;
+	historicalValue[0] = 0.0f;
+	historicalValue[1] = 0.0f;
+	historicalValue[2] = 0.0f;
+}
+
+float PIDController::execute(float desired, float actual, float actualRate) {
+
+	float error = desired - actual;
+	float derivative;
+
+	integral +=  error;
+
+	// avoid integral windup
+	if (integral < -i_max)
+	{
+		integral = -i_max;
+	}
+	else if (integral > i_max)
+	{
+		integral = i_max;
+	}
+
+	// if we are provided with a measured derivative (say from a gyroscope), it is always less noisy to use that than to compute it ourselves.
+	if ( ! std::isnan(actualRate))
+	{
+		derivative = actualRate;
+	}
+	else
+	{
+		historicalValue[2] = historicalValue[1];
+		historicalValue[1] = historicalValue[0];
+		historicalValue[0] = actual;
+
+		// Finite difference approximation gets rid of noise much better than first order derivative computation
+		derivative = ((3 * historicalValue[0]) - (4 * historicalValue[1]) + (historicalValue[2]));
+	}
+
+	float ret = ((kp * error) + (ki * integral) - (kd * derivative));
+
+	if (ret < min_output)
+	{
+		ret = min_output;
+	}
+	else if (ret > max_output)
+	{
+		ret = max_output;
+	}
+
+	return ret;
+}
