b5689f1a862f17e2847f336bccc87adf0a41a9ce
Anthony Luo <anthonyluo100@Gmail.com>
2021-11-02 19:53:41 -0400

    notes

--- Safety/Controls/Src/Controls.cpp
+++ Safety/Controls/Src/Controls.cpp
@@ -14,3 +14,8 @@
  * Literally 4 pwm values :D
 */
 
+/* Some notes
+ * Do we need to worry about tracking drift error in the IMU? -> would it be preferable if we did?
+ * Do we store current heading data? How do we store this.....
+ *  -> would we use this data later to generate better curves and splines?
+*/
