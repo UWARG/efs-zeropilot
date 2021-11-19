420f08037cc7535ce28e967bcd2be2bcf4649063
Anthony Luo <anthonyluo100@Gmail.com>
2021-11-18 19:55:41 -0500

    auto-merge

--- Safety/Controls/Src/Controls.cpp
+++ Safety/Controls/Src/Controls.cpp
@@ -3,12 +3,26 @@
 // Created by antho on 10/27/2021.
 //
 
+#include "Controls.hpp"
+
 /* Inputs:
- *      - RC inputs / PM waypoint?
- *      - Auton / RC Control
- *      - Current position relative to origin
+ * - Global position -> not sure who will track diff from prev
+ * - RC inputs
 */
 
 /* Outputs:
  *      - PWM values
 */
+
+/* Some notes
+ * Do we need to worry about tracking drift error in the IMU? -> would it be preferable if we did?
+ * Do we store current heading data? How do we store this.....
+ *  -> would we use this data later to generate better curves and splines?
+ * Define traditional XYZ coordinates: x forward, y left, z up, cw +
+*/
+
+// static volatile global position.
+
+void run_controls(){
+    // main entry point to run controls
+}
