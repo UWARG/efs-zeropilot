eb70f69ba0549770eb06c6dc0836a24d44e2a6b7
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-11-27 12:13:56 -0500

    changed channel mappings from fixed wing to quadcopter datatypes

--- Safety/AttitudeManager/Inc/AttitudeDatatypes.hpp
+++ Safety/AttitudeManager/Inc/AttitudeDatatypes.hpp
@@ -6,25 +6,34 @@
 // Output of the PID module and input to the OutputMixing module
 typedef struct
 {
-    float rollPercent;
-    float pitchPercent;
-    float rudderPercent;
-    float throttlePercent;
+    float frontLeftMotorPercent;
+    float frontRightMotorPercent;
+    float backLeftMotorPercent;
+    float backRightMotorPercent;
 
 } PID_Output_t;
 
-#ifdef SPIKE
-    constexpr static uint8_t L_TAIL_OUT_CHANNEL {0}; // Spike has ruddervators
-    constexpr static uint8_t R_TAIL_OUT_CHANNEL {1};
-#else
-    constexpr static uint8_t ELEVATOR_OUT_CHANNEL {0};
-    constexpr static uint8_t RUDDER_OUT_CHANNEL {1};
-#endif
+constexpr static uint8_t FRONT_LEFT_MOTOR_CHANNEL {0};
+constexpr static uint8_t FRONT_RIGHT_MOTOR_CHANNEL {1};
+constexpr static uint8_t BACK_LEFT_MOTOR_CHANNEL {2};
+constexpr static uint8_t BACK_RIGHT_MOTOR_CHANNEL {3};
 
-constexpr static uint8_t AILERON_OUT_CHANNEL {2};
-constexpr static uint8_t THROTTLE_OUT_CHANNEL {3};
+/*
+Commented out for now to allow for just quadcopter channel mapping
+*/
 
-constexpr static uint8_t NUM_PWM_CHANNELS {8};
+// #ifdef SPIKE
+//     constexpr static uint8_t L_TAIL_OUT_CHANNEL {0}; // Spike has ruddervators
+//     constexpr static uint8_t R_TAIL_OUT_CHANNEL {1};
+// #else
+//     constexpr static uint8_t ELEVATOR_OUT_CHANNEL {0};
+//     constexpr static uint8_t RUDDER_OUT_CHANNEL {1};
+// #endif
 
-#endif
+// constexpr static uint8_t AILERON_OUT_CHANNEL {2};
+// constexpr static uint8_t THROTTLE_OUT_CHANNEL {3};
+
+// constexpr static uint8_t NUM_PWM_CHANNELS {8};
+
+// #endif
 
