3ac81a686075c1198a90799e65dea3f996a0e0f5
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-12-26 09:44:26 -0500

    reverted changes to attitudeDatatypes.hpp in AM in Autopilot to keep PR clean

--- Autopilot/AttitudeManager/Inc/AttitudeDatatypes.hpp
+++ Autopilot/AttitudeManager/Inc/AttitudeDatatypes.hpp
@@ -6,10 +6,10 @@
 // Output of the PID module and input to the OutputMixing module
 typedef struct
 {
-    float frontLeftPercent;
-    float frontRightPercent;
-    float backLeftPercent;
-    float backRightPercent;
+    float rollPercent;
+    float pitchPercent;
+    float rudderPercent;
+    float throttlePercent;
 
 } PID_Output_t;
 
