933c03807ba2c3e07d29676307ffe67a703a7719
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-12-28 11:06:34 -0500

    renamed PPM channels accrodingly

--- Safety/Inc/safetyConfig.hpp
+++ Safety/Inc/safetyConfig.hpp
@@ -3,9 +3,9 @@
 
 // ppm channels so we can set and forget here.
 // TODO: Add channels for Arm/Disarm (later also grabber controls)
-constexpr uint8_t LONGITUDE_PPM_CHANNEL {0};
-constexpr uint8_t LATITUDE_PPM_CHANNEL {1};
-constexpr uint8_t VERTICAL_PPM_CHANNEL {2};
+constexpr uint8_t PITCH_PPM_CHANNEL {0};
+constexpr uint8_t ROLL_PPM_CHANNEL {1};
+constexpr uint8_t THROTTLE_PPM_CHANNEL {2};
 constexpr uint8_t YAW_PPM_CHANNEL {3};
 
 
