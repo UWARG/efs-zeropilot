bbef091b311bcd7bdf38a06bb15339cfd0d74851
upadhyaydhruv <dmupadhyay12@gmail.com>
2022-01-10 22:54:14 -0500

    moved sensorfusion to Common and made CMake changes

---
+++ Safety/Inc/imu.hpp
@@ -0,0 +1,49 @@
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef IMU_HPP
+#define IMU_HPP
+
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <stdint.h>
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+
+struct ImuData {
+  double gyro_x, gyro_y, gyro_z; 
+  double accel_x, accel_y, accel_z;
+}; 
+
+class Imu {
+  public: 
+    virtual void getResult(ImuData& Data) = 0; 
+}; 
+
+class MPU6050 : public Imu {
+  
+  public: 
+  
+    static Imu& getInstance(); 
+
+    MPU6050(const MPU6050*)=delete; 
+
+    void getResult(ImuData& Data); 
+
+  private:
+
+    MPU6050(); 
+
+    void ImuInit(void); 
+
+
+
+};
+
+
+
+#endif 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
