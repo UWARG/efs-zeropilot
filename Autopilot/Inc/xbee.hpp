0f296fdd98397893e2d713175dd57be96ce039b1
Aaditya Chaudhary <32117362+AadityaChaudhary@users.noreply.github.com>
2022-01-27 02:24:01 -0500

    added xbee code

---
+++ Autopilot/Inc/xbee.hpp
@@ -0,0 +1,101 @@
+#ifndef XBEE_HPP
+#define XBEE_HPP
+#include <cstdint>
+
+#define START                 0x7E
+#define TRANSMIT_TYPE         0x10
+#define RECIEVE_TYPE          0x90
+#define RESPONSE_TYPE         0x8B
+
+#define GROUND_ADDY           0x0013A20041B16D1C
+#define DRONE_ADDY            0x0013A20041B16ECD
+#define SHORT_ADDY            0xFFFE
+
+#define BROADCAST_RADIUS      0x00
+#define OPTIONS               0x00
+
+// size 
+#define TRANSMIT_PAYLOAD_SIZE 22
+#define RECIEVE_PAYLOAD_SIZE  20
+
+#define TRANSMIT_SIZE         40
+#define RECIEVE_SIZE          40
+
+
+class pogiData {
+    public:
+    uint8_t outputs[4]; 
+    uint8_t grabberPos; 
+    // 5
+    float pitch; 
+    float yaw; 
+    float roll; 
+    // 17
+    uint8_t statusDisplay;
+    // 18
+    // use to send data as needed for debugging purposes only,
+    // for changes going into the final version of this struct, make a new field in the struct
+    uint8_t spareChannels[4];
+    // 22
+    void setArmState(bool arm); // 1 bit
+    void setControlState(uint8_t state); // 2 bits
+    void setPMState(uint8_t state); // 4 bits
+
+    bool getArmState();
+    uint8_t getControlState();
+    uint8_t getPMState();
+};
+
+struct transmitFrame {
+    uint8_t startDelim = START;
+    uint16_t length = TRANSMIT_SIZE;
+    uint8_t type = TRANSMIT_TYPE;
+    uint8_t id;
+    // 5 bytes
+
+    uint64_t address64 = GROUND_ADDY;
+    uint16_t address16 = SHORT_ADDY;
+    //15
+
+    uint8_t broadcastRadius = BROADCAST_RADIUS;
+    uint8_t options = OPTIONS;
+    // 17 bytes
+
+    pogiData payload; //39 bytes
+
+    uint8_t checksum; // 40 bytes
+
+};
+
+struct responseFrame {
+    uint8_t startDelim;
+    uint16_t length;
+    uint8_t type;
+    uint8_t id;
+
+    uint16_t address16;
+    
+    uint8_t txRetryCount;
+    uint8_t deliveryStatus;
+    uint8_t discoveryStatus;
+
+    uint8_t checksum;
+};
+
+class Comms {
+    public:
+    Comms(const Comms*) = delete;
+    static Comms* GetInstance();
+
+    void transmitMessage();
+
+    pogiData tx;
+    uint8_t rxBuffer[22];
+
+    private:
+    Comms();
+    static Comms* instance;
+
+};
+
+#endif
---
+++ Autopilot/Src/xbees.cpp
@@ -0,0 +1,65 @@
+#include "xbee.hpp"
+#include "usart.h"
+
+uint8_t pogiData::getPMState() {
+    return this->statusDisplay & 00001111;
+}
+
+uint8_t pogiData::getControlState() {
+    return (this->statusDisplay & 00110000) >> 4;
+}
+
+bool pogiData::getArmState() {
+     return (this->statusDisplay & 01000000) >> 6;
+}
+
+void pogiData::setArmState(bool arm) {
+    this->statusDisplay = (this->statusDisplay & 00111111) | (arm << 6);
+}
+
+void pogiData::setControlState(uint8_t state) {
+    this->statusDisplay = (this->statusDisplay & 01001111) | ((state & 00000011) << 4);
+}
+
+void pogiData::setPMState(uint8_t state) {
+    this->statusDisplay = (this->statusDisplay & 01110000) | (state & 00001111);
+}
+
+Comms* Comms::instance = nullptr;
+
+Comms::Comms() {
+    //constructor
+}
+
+Comms* Comms::GetInstance() {
+    if(!instance) {
+        instance = new Comms;
+    }
+    return instance;
+}
+
+uint8_t calculateChecksum(uint8_t* data, uint8_t start, uint8_t end) {
+    uint8_t checksum = 0;
+    for (uint8_t i = start; i < end; i++) {
+        checksum += data[i];
+    }
+    return checksum;
+}
+
+void Comms::transmitMessage() {
+    transmitFrame frame;
+    frame.payload = this->tx;
+    frame.id = 1;
+
+    uint8_t *rawData = (uint8_t*)&frame;
+    uint8_t size = sizeof(frame);
+    rawData[size-1] = calculateChecksum(rawData, 3, size-1);
+
+    // send the data
+
+    HAL_UART_Transmit_DMA(&huart4, rawData, size);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    HAL_UART_Receive_DMA(&huart4, Comms::GetInstance()->rxBuffer, 22);
+}
