50ba041c36005a80d411e3f065a476daed6d794b
WARG Anthony <a26luo@uwaterloo.ca>
2022-04-04 17:11:06 -0400

    PID functioning

--- Safety/boardfiles/Src/main.cpp
+++ Safety/boardfiles/Src/main.cpp
@@ -167,18 +167,22 @@ int main(void)
   /* USER CODE BEGIN 2 */
   PPMChannel *ppm = new PPMChannel(MAX_PPM_CHANNELS);
   PWMChannel *pwm = new PWMChannel();
-  for(int i = 0; i<4; i++) {
+
+  bool CALIBRATE = false;
+  if (CALIBRATE) {
+for(int i = 0; i<4; i++) {
     pwm->set(i, 0);
   }
   HAL_Delay(2000);
   for(int i = 0; i<4; i++) {
-    pwm->set(i, 100);
+    // pwm->set(i, 100);
   }
   HAL_Delay(2000);
   for(int i = 0; i<4; i++) {
     pwm->set(i, 0);
   }
   HAL_Delay(2000);
+  }
   attitudeManager *attMng = new attitudeManager(ppm, pwm);
 
   interchipInit();
