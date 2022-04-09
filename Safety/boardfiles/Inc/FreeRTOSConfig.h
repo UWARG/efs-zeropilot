7efbc0ca718f8a048462466e186f762ddeb07d31
Aidan Bowers <abowers@uwaterloo.ca>
2022-04-09 11:52:11 -0400

    changes to test freeRTOS thread delay issues

--- Safety/boardfiles/Inc/FreeRTOSConfig.h
+++ Safety/boardfiles/Inc/FreeRTOSConfig.h
@@ -94,7 +94,7 @@
 #endif
 
 #define configUSE_PREEMPTION                     1
-#define configSUPPORT_STATIC_ALLOCATION          0
+#define configSUPPORT_STATIC_ALLOCATION          1
 #define configSUPPORT_DYNAMIC_ALLOCATION         1
 #define configUSE_IDLE_HOOK                      0
 #define configUSE_TICK_HOOK                      0
@@ -119,7 +119,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelete                 1
 #define INCLUDE_vTaskCleanUpResources       0
 #define INCLUDE_vTaskSuspend                1
-#define INCLUDE_vTaskDelayUntil             0
+#define INCLUDE_vTaskDelayUntil             1
 #define INCLUDE_vTaskDelay                  1
 #define INCLUDE_xTaskGetSchedulerState      1
 
