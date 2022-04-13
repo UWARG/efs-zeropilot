0b2336b585af36ab6ede0edbfb9aecb482d60d6a
Aidan Bowers <abowers@uwaterloo.ca>
2022-04-12 22:51:40 -0400

    Fix FreeRTOS threads
    - CubeMX was generating the systick_handler, but was not adding the
    osSystickHandler() method to it (which controls the os tick). This
    tickhandler was manually added and the HAL_SYSTICK_IRQHandler() which was
    there was removed as it did nothing.
    
    - If CubeMX is regenerated it will completely blow this up, so if threads
    just stop working for some reason, that is why. Make sure to revert to
    the state that this commit is in to fix it

--- Safety/boardfiles/Src/stm32f0xx_it.c
+++ Safety/boardfiles/Src/stm32f0xx_it.c
@@ -44,8 +44,6 @@
 extern SPI_HandleTypeDef hspi1;
 extern TIM_HandleTypeDef htim15;
 
-extern TIM_HandleTypeDef htim6;
-
 /******************************************************************************/
 /*            Cortex-M0 Processor Interruption and Exception Handlers         */
 /******************************************************************************/
@@ -94,9 +92,9 @@ void HardFault_Handler(void)
 //   /* USER CODE END SVC_IRQn 1 */
 // }
 
-/**
-* @brief This function handles Pendable request for system service.
-*/
+// /**
+// * @brief This function handles Pendable request for system service.
+// */
 // void PendSV_Handler(void)
 // {
 //   /* USER CODE BEGIN PendSV_IRQn 0 */
@@ -115,7 +113,9 @@ void SysTick_Handler(void)
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
   /* USER CODE END SysTick_IRQn 0 */
-  HAL_SYSTICK_IRQHandler();
+  HAL_IncTick();
+  osSystickHandler();
+  // HAL_SYSTICK_IRQHandler();
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
@@ -127,20 +127,6 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f0xx.s).                    */
 /******************************************************************************/
-
-/**
-* @brief This function handles TIM6 global interrupt.
-*/
-void TIM6_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM6_IRQn 0 */
-
-  /* USER CODE END TIM6_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim6);
-  /* USER CODE BEGIN TIM6_IRQn 1 */
-
-  /* USER CODE END TIM6_IRQn 1 */
-}
 
 /**
 * @brief This function handles TIM15 global interrupt.
