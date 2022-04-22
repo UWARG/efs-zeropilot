8f6bb1998f0d25c52110a3d550fe6b0f1ecf2160
WARG Anthony <a26luo@uwaterloo.ca>
2022-04-22 15:17:59 -0400

    Added motor initialization to RTOS

--- Safety/AttitudeManager/Src/attitudeManagerInterface.cpp
+++ Safety/AttitudeManager/Src/attitudeManagerInterface.cpp
@@ -4,12 +4,31 @@
 #include "PPM.hpp"
 #include "PWM.hpp"
 
+#include "stm32f0xx_hal.h"
+
 attitudeManager *attMng;
 
 void AttitudeManagerInterfaceInit(void) {
     PPMChannel *ppm = new PPMChannel(MAX_PPM_CHANNELS);
     PWMChannel *pwm = new PWMChannel();
     attMng = new attitudeManager(ppm, pwm);
+
+      bool CALIBRATE = true;
+  if (CALIBRATE) {
+for(int i = 0; i<=4; i++) {
+    pwm->set(i, 0);
+  }
+  HAL_Delay(2000);
+  for(int i = 0; i<=4; i++) {
+    // pwm->set(i, 100);
+    pwm->set(11, 100);
+  }
+  HAL_Delay(2000);
+  for(int i = 0; i<=11; i++) {
+    pwm->set(i, 0);
+  }
+  HAL_Delay(2000);
+  }
 }
 
 void AttitudeManagerInterfaceExecute(void)
