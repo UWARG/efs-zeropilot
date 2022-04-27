7f17e3ef92102ebcc09eac34ca5dcaa1d51fb786
Anthony Luo <anthonyluo100@Gmail.com>
2022-04-27 08:27:50 -0400

    Reflow clang-format

--- Safety/Controls/Src/Controls.cpp
+++ Safety/Controls/Src/Controls.cpp
@@ -37,8 +37,7 @@ static PID_Output_t PID_Out;
 // PID consts
 // =======================================================
 const int pid_abs_max = 100;
-const int max_i_windup =
-    5;  // ? not sure if we need specifics for each angle -> especially yaw
+const int max_i_windup = 5;  // ? not sure if we need specifics for each angle
 
 const float roll_kp = 0.175;
 const float roll_ki = 0;
@@ -58,8 +57,7 @@ const int PID_method = 0;
  * Code
  **********************************************************************************************************************/
 
-PID_Output_t *runControlsAndGetPWM(Instructions_t *instructions,
-                                   SFOutput_t *SF_pos) {
+PID_Output_t *runControlsAndGetPWM(Instructions_t *instructions, SFOutput_t *SF_pos) {
   // to use or not to use pointers?
   curr_sf = *SF_pos;
 
@@ -69,35 +67,25 @@ PID_Output_t *runControlsAndGetPWM(Instructions_t *instructions,
 
   // PIDController controller(float _kp, float _ki, float _kd, float _i_max,
   // float _min_output, float _max_output);
-  PIDController pid_roll{roll_kp,      roll_ki,      roll_kd,
-                         max_i_windup, -pid_abs_max, pid_abs_max};
+  PIDController pid_roll{roll_kp, roll_ki, roll_kd, max_i_windup, -pid_abs_max, pid_abs_max};
 
-  PIDController pid_pitch{pitch_kp,     pitch_ki,     pitch_kd,
-                          max_i_windup, -pid_abs_max, pid_abs_max};
+  PIDController pid_pitch{pitch_kp, pitch_ki, pitch_kd, max_i_windup, -pid_abs_max, pid_abs_max};
 
-  PIDController pid_yaw{yaw_kp,       yaw_ki,       yaw_kd,
-                        max_i_windup, -pid_abs_max, pid_abs_max};
+  PIDController pid_yaw{yaw_kp, yaw_ki, yaw_kd, max_i_windup, -pid_abs_max, pid_abs_max};
 
   // get PID result
-  float roll = pid_roll.execute(
-      instructions->input1, curr_sf.roll,
-      curr_sf.rollRate);  // change yaw and yawRate to correct measurement.
-  // yawRate may not be usable. If you don't put in yawRate PID will
-  // automatically calculate derivative
-
-  float pitch =
-      pid_pitch.execute(instructions->input2, curr_sf.pitch, curr_sf.pitchRate);
+  float roll = pid_roll.execute(instructions->input1, curr_sf.roll, curr_sf.rollRate);
+  float pitch = pid_pitch.execute(instructions->input2, curr_sf.pitch, curr_sf.pitchRate);
 
   // yaw is now going to target a specific yawrate.
   // FIXME: double check that the yawRate matches direction from stick
   float yaw = pid_yaw.execute(instructions->input4, curr_sf.yawRate);
-
   float throttle = instructions->input3;  //
 
   // mix the PID's.
-  PID_Out.backLeftMotorPercent   = throttle - roll - pitch + yaw;
-  PID_Out.frontLeftMotorPercent  = throttle - roll + pitch - yaw;
-  PID_Out.backRightMotorPercent  = throttle + roll - pitch - yaw;
+  PID_Out.backLeftMotorPercent = throttle - roll - pitch + yaw;
+  PID_Out.frontLeftMotorPercent = throttle - roll + pitch - yaw;
+  PID_Out.backRightMotorPercent = throttle + roll - pitch - yaw;
   PID_Out.frontRightMotorPercent = throttle + roll + pitch + yaw;
   return &PID_Out;
 }
